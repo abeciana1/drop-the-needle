26140814612d974c375d282fa5bf871c23bc5f23
Alex <alex.beciana@gmail.com>
2023-04-25 15:37:23 -0400

    Removed unused code

--- components/common/Navigation/NavItem/__test__/index.test.tsx
+++ components/common/Navigation/NavItem/__test__/index.test.tsx
@@ -1,18 +1,14 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+// import userEvent from '@testing-library/user-event'
 import NavItem from '..'
-import { RouterContext } from 'next/dist/shared/lib/router-context';
-import { createMockRouter } from '@/utils/test-utils/createMockRouter'
 
 const renderComponent = () => {
     render(
-        <RouterContext.Provider value={createMockRouter({})}>
-            <NavItem
-                href='/about'
-                linkText='about'
-                color='altBlack'
-            />
-        </RouterContext.Provider>
+        <NavItem
+            href='/about'
+            linkText='about'
+            color='altBlack'
+        />
     )
 
     const link = screen.getByRole('link', {
