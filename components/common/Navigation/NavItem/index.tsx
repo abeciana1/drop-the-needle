bd62dbf9e21c08eea7f7876a868ba3745cbddd4d
Alex <alex.beciana@gmail.com>
2023-04-26 14:18:17 -0400

    Deleted boilerplate assets

--- components/common/Navigation/NavItem/index.tsx
+++ components/common/Navigation/NavItem/index.tsx
@@ -15,10 +15,10 @@ const NavItem = ({
             <Link
                 href={`/${encodeURIComponent(href)}`}
                 title={`Navigate to ${linkText} page`}
-                className={cx('flex flex-col font-medium text-2xl anim-text z-50', {
-                    ['hover:text-jaffa jaffa hover-underline-animation text-jaffa after:bg-jaffa']: color === 'jaffa',
-                    ['hover:text-vermillion vermillion hover-underline-animation text-vermillion after:bg-vermillion']: color === 'vermillion',
-                    ['hover:text-altGreen altGreen hover-underline-animation text-altGreen after:bg-altGreen']: color === 'altGreen'
+                className={cx('flex flex-col font-medium text-2xl anim-text z-50 hover-underline-animation', {
+                    ['hover:text-jaffa jaffa text-jaffa after:bg-jaffa']: color === 'jaffa',
+                    ['hover:text-vermillion vermillion  text-vermillion after:bg-vermillion']: color === 'vermillion',
+                    ['hover:text-altGreen altGreen text-altGreen after:bg-altGreen']: color === 'altGreen'
                 })}
             >
                 {linkText}
