aed6dd9a4423b823f9361594a70977f91c390ac2
Alex <alex.beciana@gmail.com>
2023-04-27 21:59:10 -0400

    Added conditional render of hamburger button

--- components/common/Navigation/NavBar/index.tsx
+++ components/common/Navigation/NavBar/index.tsx
@@ -75,13 +75,15 @@ const NavBar = () => {
                         </div>
                     </React.Fragment>
                 }
-                <div data-testid='hamburger-btn' className='absolute top-5 right-5'>
-                    <Hamburger
-                        toggled={isOpen}
-                        toggle={setOpen}
-                        color='#343434'
-                    />
-                </div>
+                {(isMobile || isTablet) &&
+                    <div data-testid='hamburger-btn' className='absolute top-5 right-5'>
+                        <Hamburger
+                            toggled={isOpen}
+                            toggle={setOpen}
+                            color='#343434'
+                        />
+                    </div>
+                }
             </nav>
         </React.Fragment>
     )
