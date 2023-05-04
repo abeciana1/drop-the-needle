73976e6ea3042ed04ebc1140b452ef21b40f4ba6
Alex <alex.beciana@gmail.com>
2023-05-04 15:56:45 -0400

    Added NextSEO to Signin page

---
+++ pages/signin.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NextSeo } from 'next-seo'
+
+const SigninPage = () => {
+
+    return (
+        <React.Fragment>
+            <NextSeo
+                title='Drop The Needle'
+            />
+        </React.Fragment>
+    )
+}
+
+export default SigninPage
