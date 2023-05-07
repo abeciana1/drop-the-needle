bc578f645b70465a55adb9636a33070e65b76884
Alex <alex.beciana@gmail.com>
2023-05-07 12:45:50 -0400

    Exported DefaultButton comp

--- components/common/index.tsx
+++ components/common/index.tsx
@@ -10,3 +10,4 @@ export {
 export {default as Feature} from '@/components/common/Feature'
 export {default as Footer} from '@/components/common/Footer'
 export {default as SEO} from '@/components/common/SEO'
+export { DefaultButton } from '@/components/common/Button'
