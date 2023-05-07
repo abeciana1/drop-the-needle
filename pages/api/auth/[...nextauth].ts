a076cf6fa7dd4ab11bdaf5a6b84bb34712bfb1da
Alex <alex.beciana@gmail.com>
2023-05-06 20:31:33 -0400

    Added google auth provider secrets

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,2 +1,11 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+
+const options = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        })
+    ]
+}
