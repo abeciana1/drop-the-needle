ce5cf4047e8eee8b7ad8c4d67fc989b476db8ddc
Alex <alex.beciana@gmail.com>
2023-05-16 21:57:58 -0400

    Removed comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { SessionProvider, getSession } from "next-auth/react"
-// import { GetServerSideProps } from 'next'
+import { SessionProvider } from "next-auth/react"
+
 // import { PrismaClient } from '@prisma/client'
 // const prisma = new PrismaClient()
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  // console.log('session', session)
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
