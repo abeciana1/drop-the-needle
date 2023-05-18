26b1dd217b0c9564a35964297c9d504958b80f88
Alex <alex.beciana@gmail.com>
2023-05-17 22:43:11 -0400

    Added <ul> and <li> options

--- components/common/Navigation/AccountDD/index.tsx
+++ components/common/Navigation/AccountDD/index.tsx
@@ -8,6 +8,11 @@ const AccountDD = () => {
             <div className='flex flex-col font-medium text-2xl anim-text hover-underline-animation hover:text-ceruleanBlue ceruleanBlue text-ceruleanBlue after:bg-ceruleanBlue'>
                 My Account
             </div>
+            <ul className="absolute">
+                <li>Dashboard</li>
+                <li>My Power Hours</li>
+                <li>Signout</li>
+            </ul>
         </li>
     )
 }
