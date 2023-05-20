1c5a5f0a7fcc0dbda38dc775739e4607582f8055
Alex <alex.beciana@gmail.com>
2023-05-20 19:17:34 -0400

    Created initial render of dashboard page

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { SEO } from '@/components/common'
+
+const DashboardIdxPage = () => {
+
+    return (
+        <React.Fragment>
+            <SEO
+                title='Dashboard'
+            />
+
+        </React.Fragment>
+    )
+}
+
+export default DashboardIdxPage
