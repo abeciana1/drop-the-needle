3f125054f18bcd0558d46ace40dc941558e533ba
Alex <alex.beciana@gmail.com>
2023-05-20 18:44:46 -0400

    Removed navbar and useSession rom app && _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,10 @@ import { SessionProvider } from "next-auth/react"
 // import { PrismaClient } from '@prisma/client'
 // const prisma = new PrismaClient()
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
+
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   )
