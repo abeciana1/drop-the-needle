2a18e165cc3ef688f8b5993c5360207f28c2dc64
Alex <alex.beciana@gmail.com>
2023-05-20 12:26:23 -0400

    Added conditional dropdown opening rendering:

--- components/common/Navigation/AccountDD/index.tsx
+++ components/common/Navigation/AccountDD/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { signOut } from "next-auth/react"
 import useResponsiveness from '@/hooks/useResponsiveness'
 import cx from 'classnames'
 
 const AccountDD = () => {
+    const [ open, setOpen ] = useState(false)
 
     const {
         isMobile,
@@ -18,38 +19,40 @@ const AccountDD = () => {
 
     return (
         <li className="list-none z-50 relative">
-            <div className='flex flex-col font-medium text-2xl anim-text hover-underline-animation hover:text-ceruleanBlue ceruleanBlue text-ceruleanBlue after:bg-ceruleanBlue'>
+            <button onClick={() => setOpen(!open)} className='flex flex-col font-medium text-2xl anim-text hover-underline-animation hover:text-ceruleanBlue ceruleanBlue text-ceruleanBlue after:bg-ceruleanBlue'>
                 My Account
-            </div>
-            <ul className={cx('font-medium text-lg',{
-                ["space-y-3 mt-3 ml-3"]: isTablet || isMobile,
-                ["absolute mt-0 ml-0 space-y-0.5 bg-transparent shadow-xl p-2 right-0 w-44 rounded-lg text-right"]: isDesktop
-            })}>
-                <li>
-                    <Link
-                        href={encodeURI('/dashboard')}
-                        className='hover:underline decoration-2 underline-offset-4'
-                    >
-                        Dashboard
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        href={encodeURI('/dashboard/power-hours')}
-                        className='hover:underline decoration-2 underline-offset-4'
-                    >
-                        My Power Hours
-                    </Link>
-                </li>
-                <li>
-                    <button
-                        onClick={handleSignOut}
-                        className='hover:underline decoration-2 underline-offset-4'
-                    >
-                        Signout
-                    </button>
-                </li>
-            </ul>
+            </button>
+            {open &&
+                <ul className={cx('font-medium text-lg',{
+                    ["space-y-3 mt-3 ml-3"]: isTablet || isMobile,
+                    ["absolute mt-0 ml-0 space-y-0.5 bg-transparent shadow-xl p-2 right-0 w-44 rounded-lg text-right"]: isDesktop
+                })}>
+                    <li>
+                        <Link
+                            href={encodeURI('/dashboard')}
+                            className='hover:underline decoration-2 underline-offset-4'
+                        >
+                            Dashboard
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href={encodeURI('/dashboard/power-hours')}
+                            className='hover:underline decoration-2 underline-offset-4'
+                        >
+                            My Power Hours
+                        </Link>
+                    </li>
+                    <li>
+                        <button
+                            onClick={handleSignOut}
+                            className='hover:underline decoration-2 underline-offset-4'
+                        >
+                            Signout
+                        </button>
+                    </li>
+                </ul>
+            }
         </li>
     )
 }
