e635b21795e83bdf6c0095b0f03061f395593458
Alex <alex.beciana@gmail.com>
2023-05-21 19:14:48 -0400

    Created DrawerItemLink && DrawerItemButton comps

---
+++ components/account/AuthNavigation/DrawerItem/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export const DrawerItemLink = () => {
+
+    return (
+        <Link></Link>
+    )
+}
+
+export const DrawerItemButton = () => {
+
+    return (
+        <button></button>
+    )
+}
