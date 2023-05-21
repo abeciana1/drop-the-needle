15db8027a5eabfabaa4676b996cba52530771bb2
Alex <alex.beciana@gmail.com>
2023-05-21 19:15:14 -0400

    Removed space

--- components/account/AuthNavigation/DrawerNav/__test__/index.test.tsx
+++ components/account/AuthNavigation/DrawerNav/__test__/index.test.tsx
@@ -2,7 +2,6 @@ import { DrawerNav } from '@/components/account/AuthNavigation'
 import { render, screen } from '@testing-library/react'
 
 test('DrawerNav component renders without issues', () => {
-
     render(<DrawerNav/>)
     let nav = screen.getByRole('navigation')
     expect(nav).toBeInTheDocument()
