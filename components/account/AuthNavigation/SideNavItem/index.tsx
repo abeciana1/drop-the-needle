233bbb93713582edf9ece3d4e77a6e0662667941
Alex <alex.beciana@gmail.com>
2023-05-23 21:30:42 -0400

    Added tooltip rendering

--- components/account/AuthNavigation/SideNavItem/index.tsx
+++ components/account/AuthNavigation/SideNavItem/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import {
     NavItemIconI,
@@ -9,18 +10,26 @@ export const SideNavItemLink = ({
     linkText,
     icon
 }: NavItemIconI) => {
-
+    const [ hover, setHover ] = useState(false)
     const Icon = icon as React.ElementType
 
     return (
-        <li className="list-none">
+        <li 
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            className="list-none flex items-center"
+        >
             <Link
                 href={`/${encodeURI(href)}`}
                 className='text-altWhite text-2xl font-medium'
             >
                 <Icon size={'2.25rem'}/>
             </Link>
-            {/* {linkText} */}
+            {hover &&
+                <div 
+                    className='absolute ml-16 font-medium text-altWhite bg-altBlack px-2 py-1 rounded-lg'
+                >{linkText}</div>
+            }
         </li>
     )
 }
@@ -30,18 +39,26 @@ export const SideNavItemButton = ({
     onClick,
     icon
 }: OnClickIconButtonI) => {
-
+    const [ hover, setHover ] = useState(false)
     const Icon = icon as React.ElementType
 
     return (
-        <li className="list-none">
+        <li
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            className="list-none flex items-center"
+        >
             <button 
                 onClick={onClick}
                 className='text-altWhite text-2xl font-medium'
             >
-                {/* {text} */}
                 <Icon size={'2.25rem'} />
             </button>
+            {hover &&
+                <div 
+                    className='absolute ml-16 font-medium text-altWhite bg-altBlack px-2 py-1 rounded-lg'
+                >{text}</div>
+            }
         </li>
     )
 }
