653c87b933d75a91aa9b8c5c50c61b497b00faf4
Alex <alex.beciana@gmail.com>
2023-05-24 19:33:42 -0400

    Created file to for prisma use

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,6 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 
-// import { PrismaClient } from '@prisma/client'
-// const prisma = new PrismaClient()
-
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
