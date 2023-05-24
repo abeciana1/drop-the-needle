734bc474101a195d802367e6f0fb4b8fe85129d8
Alex <alex.beciana@gmail.com>
2023-05-24 19:24:09 -0400

    Fetched session data server side

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import { Fragment } from 'react'
 import { SEO } from '@/components/common'
 import { SideNav } from '@/components/account'
+import { NextPageContext } from 'next';
+import { getSession } from "next-auth/react"
 
 const DashboardIdxPage = () => {
 
@@ -15,3 +17,11 @@ const DashboardIdxPage = () => {
 }
 
 export default DashboardIdxPage
+
+export const getServerSideProps = async (context: NextPageContext) => {
+    let session = await getSession(context)
+    console.log('data', session)
+    return {
+        props: session
+    }
+}
