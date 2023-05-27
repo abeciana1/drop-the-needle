14e40f88707ddf8600a54d2a43c254249238845b
Alex <alex.beciana@gmail.com>
2023-05-27 19:07:11 -0400

    Imported DashPageLayout on dashboard page && removed old code

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -3,7 +3,8 @@ import {
     SEO,
     PlaylistCard,
     ComponentMargin,
-    WavySection
+    WavySection,
+    DashPageLayout
 } from '@/components/common'
 import { SideNav } from '@/components/account'
 import axios from 'axios';
@@ -25,47 +26,44 @@ const DashboardIdxPage = ({data}: any) => {
             <SEO
                 title='Dashboard'
             />
-            <section className='flex flex-row'>
-                <SideNav/>
-                <main className='w-full'>
-                    <ComponentMargin>
-                        <H1 color={2} text={`Welcome back, ${name}`}/>
-                    </ComponentMargin>
-                    <WavySection color='jaffa-200' type={1} />
-                    <ComponentMargin bgColor='jaffa-200'>
-                        <H2 color={0} text={'My Hosted Power Hours'} />
-                        <section className='py-5 grid grid-cols-3'>
-                            {hosted.map(({powerHour}: PlaylistCardI) => (
-                                <PlaylistCard
-                                    key={powerHour.id}
-                                    id={powerHour.id}
-                                    title={powerHour.title}
-                                    cover_image={powerHour.cover_image}
-                                />
-                            ))}
-                        </section>
-                    </ComponentMargin>
-                    <WavySection color='jaffa-200' type={2} bgColor='altGreen-300' />
-                    <ComponentMargin bgColor='altGreen-300'>
-                        <H2 color={0} text={'Participation'} />
-                        <section className='py-5 grid grid-cols-3'>
-                            {participants.map(({powerHour}: PlaylistCardI) => (
-                                <PlaylistCard
-                                    key={powerHour.id}
-                                    id={powerHour.id}
-                                    title={powerHour.title}
-                                    cover_image={powerHour.cover_image}
-                                />
-                            ))}
-                        </section>
-                    </ComponentMargin>
-                    <WavySection color='altGreen-300' type={3} bgColor='vermillion-200' />
-                    <ComponentMargin bgColor='vermillion-200'>
-                        <H2 text={'Invitations'} />
-                    </ComponentMargin>
-                </main>
-            </section>
-            
+            <DashPageLayout>
+
+                <ComponentMargin>
+                    <H1 color={2} text={`Welcome back, ${name}`}/>
+                </ComponentMargin>
+                <WavySection color='jaffa-200' type={1} />
+                <ComponentMargin bgColor='jaffa-200'>
+                    <H2 color={0} text={'My Hosted Power Hours'} />
+                    <section className='py-5 grid grid-cols-3'>
+                        {hosted.map(({powerHour}: PlaylistCardI) => (
+                            <PlaylistCard
+                                key={powerHour.id}
+                                id={powerHour.id}
+                                title={powerHour.title}
+                                cover_image={powerHour.cover_image}
+                            />
+                        ))}
+                    </section>
+                </ComponentMargin>
+                <WavySection color='jaffa-200' type={2} bgColor='altGreen-300' />
+                <ComponentMargin bgColor='altGreen-300'>
+                    <H2 color={0} text={'Participation'} />
+                    <section className='py-5 grid grid-cols-3'>
+                        {participants.map(({powerHour}: PlaylistCardI) => (
+                            <PlaylistCard
+                                key={powerHour.id}
+                                id={powerHour.id}
+                                title={powerHour.title}
+                                cover_image={powerHour.cover_image}
+                            />
+                        ))}
+                    </section>
+                </ComponentMargin>
+                <WavySection color='altGreen-300' type={3} bgColor='vermillion-200' />
+                <ComponentMargin bgColor='vermillion-200'>
+                    <H2 text={'Invitations'} />
+                </ComponentMargin>
+            </DashPageLayout>
         </Fragment>
     )
 }
