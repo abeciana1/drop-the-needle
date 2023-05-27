3e38b44b7f48ede23413b085c86006746643270f
Alex <alex.beciana@gmail.com>
2023-05-26 23:05:38 -0400

    Commented out getStaticPaths func && Removed unused imports

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,8 +1,6 @@
 import { Fragment } from 'react'
 import { SEO } from '@/components/common'
 import { SideNav } from '@/components/account'
-import { NextPageContext } from 'next';
-import { getSession } from "next-auth/react"
 import axios from 'axios';
 
 // todo create endpoint for user data mapping and gathering
@@ -21,20 +19,20 @@ const DashboardIdxPage = ({data}: any) => {
 
 export default DashboardIdxPage
 
-export const getStaticPaths = async () => {
-    let { data } = await axios.get('http://localhost:3000/api/user/get-dashboard')
-    let hostedPaths = data?.user?.hosted.map((hosted: any) => {
-        return hosted?.powerHour?.id
-    })
-    let participantPaths = data?.user?.participants.map((participant: any) => {
-        return participant?.powerHour?.id
-    })
-    const paths = hostedPaths.concat(participantPaths)
-    return {
-        paths,
-        fallback: true
-    }
-}
+// export const getStaticPaths = async () => {
+//     let { data } = await axios.get('http://localhost:3000/api/user/get-dashboard')
+//     let hostedPaths = data?.user?.hosted.map((hosted: any) => {
+//         return hosted?.powerHour?.id
+//     })
+//     let participantPaths = data?.user?.participants.map((participant: any) => {
+//         return participant?.powerHour?.id
+//     })
+//     const paths = hostedPaths.concat(participantPaths)
+//     return {
+//         paths,
+//         fallback: true
+//     }
+// }
 
 export const getStaticProps = async () => {
     let { data } = await axios.get('http://localhost:3000/api/user/get-dashboard')
