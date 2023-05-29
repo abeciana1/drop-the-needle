0ba8f2f74f3adb726be221f588a287c72f4d819e
Alex <alex.beciana@gmail.com>
2023-05-28 23:16:53 -0400

    Added powerhour api dir and get-hosted endpoint

---
+++ pages/api/powerhour/get-hosted.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/hooks/prisma'
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+
+}
+
+export default handler
