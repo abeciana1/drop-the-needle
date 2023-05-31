7ed3943025a044e364b0613300753959fe72f217
Alex <alex.beciana@gmail.com>
2023-05-31 18:16:23 -0400

    Created get-participants endpoint to return participants power hours

---
+++ pages/api/powerhour/get-participants.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/hooks/prisma'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    let user = await prisma?.user?.findFirst({
+        where: {
+            email: req?.body?.params
+        },
+        select: {
+            id: true
+        }
+    })
+    let powerHours = await prisma?.participant?.findMany({
+        where: {
+            userId: user?.id
+        },
+        include: {
+            powerHour: true
+        }
+    })
+    res.status(200).json({ powerHours })
+}
+
+export default handler
