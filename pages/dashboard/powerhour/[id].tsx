27c3b83d19326156ec1cd5063b8a193587269079
Alex <alex.beciana@gmail.com>
2023-05-31 22:18:06 -0400

    Added DashPageLayout and SEO comps

--- pages/dashboard/powerhour/[id].tsx
+++ pages/dashboard/powerhour/[id].tsx
@@ -1,9 +1,24 @@
+import {
+    DashPageLayout,
+    SEO
+} from '@/components/common'
 
 const PowerHourDynamic = () => {
 
     return (
-        <></>
+        <>
+            <SEO />
+            <DashPageLayout>
+                <section className="">
+
+                </section>
+            </DashPageLayout>
+        </>
     )
+}
+
+export const getStaticPaths = () => {
+    
 }
 
 export default PowerHourDynamic
