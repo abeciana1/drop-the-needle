88c6dca750aed054ca3490d27edb5e0e55bf3688
Alex <alex.beciana@gmail.com>
2023-06-01 21:53:48 -0400

    Exported component interface

---
+++ interfaces/Component/index.ts
@@ -0,0 +1,68 @@
+import React from 'react'
+import {
+    ImageType,
+    DashPowerHourType
+} from '@/types';
+
+export interface ChildrenI {
+    children: React.ReactElement | React.ReactElement[];
+}
+
+export interface HeroSectionI {
+    heading: string;
+    bodyTagline: string;
+    image: ImageType;
+    ctaButton: boolean;
+    ctaText?: string;
+    ctaLink?: string;
+    ctaColor?: string;
+}
+
+export interface FeatureI {
+    title: string;
+    body: string;
+    icon: React.ElementType;
+    color?: string;
+}
+
+export interface WavySectionI {
+    color: string;
+    bgColor?: string;
+    type: 1 | 2 | 3 | 4;
+}
+
+export interface ComponentMarginI {
+    children: React.ReactElement | React.ReactElement[];
+    bgColor?: string;
+}
+
+export interface SeoI {
+    title?: string;
+    description?: string;
+    noIndex?: boolean;
+    noFollow?: boolean;
+}
+
+export interface AccountI {
+    name: string;
+    email: string;
+    image: string;
+}
+
+
+export interface PlaylistCardI {
+    powerHour: DashPowerHourType;
+}
+
+export interface StyledHeadingI {
+    text: string;
+    color?: number;
+}
+
+export interface FooterI {
+    bgColor: string;
+}
+
+export interface CommonPageLayoutI extends ChildrenI {
+    footerColor?: string
+}
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,9 +1,3 @@
-import React from 'react'
-import {
-    ImageType,
-    DashPowerHourType
-} from '@/types';
-
 export type {
     NavItemI,
     NavItemIconI,
@@ -20,65 +14,16 @@ export type {
     ToggleButtonI
 } from '@/interfaces/Button'
 
-export interface ChildrenI {
-    children: React.ReactElement | React.ReactElement[];
-}
-
-export interface HeroSectionI {
-    heading: string;
-    bodyTagline: string;
-    image: ImageType;
-    ctaButton: boolean;
-    ctaText?: string;
-    ctaLink?: string;
-    ctaColor?: string;
-}
-
-export interface FeatureI {
-    title: string;
-    body: string;
-    icon: React.ElementType;
-    color?: string;
-}
-
-export interface WavySectionI {
-    color: string;
-    bgColor?: string;
-    type: 1 | 2 | 3 | 4;
-}
-
-export interface ComponentMarginI {
-    children: React.ReactElement | React.ReactElement[];
-    bgColor?: string;
-}
-
-export interface SeoI {
-    title?: string;
-    description?: string;
-    noIndex?: boolean;
-    noFollow?: boolean;
-}
-
-export interface AccountI {
-    name: string;
-    email: string;
-    image: string;
-}
-
-
-export interface PlaylistCardI {
-    powerHour: DashPowerHourType;
-}
-
-export interface StyledHeadingI {
-    text: string;
-    color?: number;
-}
-
-export interface FooterI {
-    bgColor: string;
-}
-
-export interface CommonPageLayoutI extends ChildrenI {
-    footerColor?: string
-}
+export type {
+    ChildrenI,
+    HeroSectionI,
+    FeatureI,
+    WavySectionI,
+    ComponentMarginI,
+    SeoI,
+    AccountI,
+    PlaylistCardI,
+    StyledHeadingI,
+    FooterI,
+    CommonPageLayoutI
+} from '@/interfaces/Component'
