495a6b696d4fb7756457e81598100890af616137
Alex <alex.beciana@gmail.com>
2023-06-04 18:42:39 -0400

    Removed outline class

--- components/account/Form/SingleSelect/index.tsx
+++ components/account/Form/SingleSelect/index.tsx
@@ -43,7 +43,7 @@ const SingleSelectField = ({
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
             >
-                <Listbox.Options className="z-50 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                <Listbox.Options className="z-50 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base ring-1 ring-black ring-opacity-5 sm:text-sm">
                 {dataSource.map((data: any, dataIdx: number) => (
                     <Listbox.Option
                     key={dataIdx}
