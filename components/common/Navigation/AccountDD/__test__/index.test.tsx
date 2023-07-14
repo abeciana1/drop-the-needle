b6ea9de48e80a59eb18b9a0c730bcd25c02e1c34
Alex <alex.beciana@gmail.com>
2023-07-14 17:03:28 -0400

    Started unit testing for AccountDD comp

---
+++ components/common/Navigation/AccountDD/__test__/index.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import { AccountDD } from '@/components/common'
+
+describe('AccountDD', () => {
+    test('> renders \'My Account\' button', () => {
+        screen.debug()
+    })
+})
