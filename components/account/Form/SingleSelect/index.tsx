7558ebd5913eff42a13bce30f06cc03b89a91611
Alex <alex.beciana@gmail.com>
2023-07-19 14:39:37 -0400

    Added data-testid attr

--- components/account/Form/SingleSelect/index.tsx
+++ components/account/Form/SingleSelect/index.tsx
@@ -20,7 +20,7 @@ const SingleSelectField = ({
     const Icon = icon as React.ElementType
 
     return (
-        <div className="w-full mx-auto">
+        <div data-testid='select-field' className="w-full mx-auto">
         <label className="sr-only">{ labelText }</label>
         <Listbox value={selectedValue} onChange={setSelectedValue}>
             <div className="relative">
