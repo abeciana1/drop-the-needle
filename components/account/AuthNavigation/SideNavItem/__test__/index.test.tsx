b4dfa2faaef82236d76b4338f4fca300f9705b43
Alex <alex.beciana@gmail.com>
2023-07-19 17:30:38 -0400

    Added SideNavItemButton comp tests

--- components/account/AuthNavigation/SideNavItem/__test__/index.test.tsx
+++ components/account/AuthNavigation/SideNavItem/__test__/index.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react'
-import { SideNavItemLink } from '@/components/account'
-import { AiFillLayout } from 'react-icons/ai'
+import {
+    SideNavItemLink,
+    SideNavItemButton
+} from '@/components/account'
+import { AiFillLayout, AiOutlineLogout } from 'react-icons/ai'
+import userEvent from '@testing-library/user-event'
 
-const renderedComponent = () => {
+const handleSignOut = jest.fn();
+
+const renderedLink = () => {
     render(
         <SideNavItemLink
             href='dashboard'
@@ -12,13 +18,47 @@ const renderedComponent = () => {
     )
 }
 
+const renderedBtn = () => {
+    render(
+        <SideNavItemButton
+            text='Signout'
+            icon={AiOutlineLogout}
+            onClick={handleSignOut}
+        />
+    )
+}
+
 describe('SideNavItemLink', () => {
     test('> renders successfully', () => {
-        renderedComponent()
+        renderedLink()
         const link = screen.getByRole('link', {
             name: /dashboard/i
         })
         expect(link).toBeInTheDocument()
         expect(link).toHaveAttribute('href', '/dashboard')
+    })
+    test('> tooltip visible on hover', async () => {
+        renderedLink()
+        const link = screen.getByRole('link', {
+            name: /dashboard/i
+        })
+        await userEvent.hover(link)
+        const tooltip = await screen.findByTestId('tooltip')
+        expect(tooltip).toBeInTheDocument()
+    })
+})
+
+describe('SideNavItemButton', () => {
+    test.only('> renders successfully', async () => {
+        renderedBtn()
+        screen.debug()
+        const button = screen.getByTestId('Signout')
+        expect(button).toBeInTheDocument()
+        await userEvent.hover(button)
+        const tooltip = await screen.findByTestId('tooltip')
+        expect(tooltip).toBeInTheDocument()
+        await userEvent.click(button)
+        expect(handleSignOut).toBeCalled()
+        expect(handleSignOut).toBeCalledTimes(1)
     })
 })
