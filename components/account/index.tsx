569f0ef7c218c9b61513c21acd1103504f2e5145
Alex <alex.beciana@gmail.com>
2023-07-21 16:21:49 -0400

    Exported TrackList comp from account dir

--- components/account/index.tsx
+++ components/account/index.tsx
@@ -9,3 +9,4 @@ export {
 } from '@/components/account/Form'
 
 export { default as AccordionDataList } from '@/components/account/AccordionDataList'
+export { default as TrackList } from '@/components/account/Playlist/List'
