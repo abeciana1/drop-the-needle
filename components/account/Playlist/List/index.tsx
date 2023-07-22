17c74c6fb55aab44900cbdbd00b2745d83cfd4c1
Alex <alex.beciana@gmail.com>
2023-07-22 10:19:42 -0400

    Conditionally rendered track list based on length of songs data

--- components/account/Playlist/List/index.tsx
+++ components/account/Playlist/List/index.tsx
@@ -6,7 +6,10 @@ const TrackList = ({
 
     return(
         <section>
-            <ul></ul>
+            <div className="font-medium">(Click on the track to expand details)</div>
+            {songs?.length > 0 &&
+                <ul className="border-altBlack border-4 rounded-lg"></ul>
+            }
         </section>
     )
 }
