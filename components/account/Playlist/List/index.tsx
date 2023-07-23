aaa119441fe1067c05ffa090fdddbf38afb30bdf
Alex <alex.beciana@gmail.com>
2023-07-22 22:10:18 -0400

    Mapped over songs and returned Track comp

--- components/account/Playlist/List/index.tsx
+++ components/account/Playlist/List/index.tsx
@@ -1,4 +1,5 @@
-import { TrackListI } from '@/interfaces'
+import { TrackListI, SongI } from '@/interfaces'
+import { Track } from '@/components/account'
 
 const TrackList = ({
     songs
@@ -8,7 +9,11 @@ const TrackList = ({
         <section>
             <div className="font-medium">(Click on the track to expand details)</div>
             {songs?.length > 0 &&
-                <ul className="border-altBlack border-4 rounded-lg"></ul>
+                <ul className="border-altBlack border-4 rounded-lg">
+                    {songs?.map((song: SongI) => {
+                        return <Track key={song?.id} song={song} user={song?.participant?.user?.name} />
+                    })}
+                </ul>
             }
         </section>
     )
