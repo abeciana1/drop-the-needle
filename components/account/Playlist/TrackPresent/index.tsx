f43eab03e6d7c826eeb8e91de09b7bce01687ac0
Alex <alex.beciana@gmail.com>
2023-07-24 17:51:38 -0400

    Added functions for ReactPlayer comp embedding and conversion

--- components/account/Playlist/TrackPresent/index.tsx
+++ components/account/Playlist/TrackPresent/index.tsx
@@ -1,9 +1,41 @@
 import ReactPlayer from 'react-player/lazy'
 import { TrackPresentI } from '@/interfaces'
 
-const TrackPresent = ({ link }: TrackPresentI) => {
+const TrackPresent = ({
+    link,
+    startTime,
+    endTime
+}: TrackPresentI) => {
+
+    const timeToIntConvert = (time: string) => {
+        let splitTime = time.split(":")
+        let convertedTime = (parseInt(splitTime[0]) * 60) + parseInt(splitTime[1])
+        return convertedTime
+    }
+
+    const convertedStartTime = timeToIntConvert(startTime)
+    const convertedEndTime = timeToIntConvert(endTime)
+
+    const stateMonitor = () => {
+        let duration = convertedEndTime - convertedStartTime
+        return duration
+    }
+
+    const embedLink = () => {
+        let youtubeId = link.split("=")
+        let convertedLink = `https://www.youtube.com/embed/${youtubeId[1]}?start=${convertedStartTime}&end=${convertedEndTime}`
+        return convertedLink
+    }
+
     return (
-        <div></div>
+        <>
+            {/* <ReactPlayer
+                url={embedLink}
+                onPlay={stateMonitor}
+                controls={true}
+                className="mx-auto"
+            /> */}
+        </>
     )
 }
 
