62f90e44c07e7215b55f8f37bd6a3a4165d23ad3
Alex <alex.beciana@gmail.com>
2023-07-24 16:32:06 -0400

    Exported TrackPresent from Playlist root dir

--- components/account/Playlist/index.tsx
+++ components/account/Playlist/index.tsx
@@ -1,2 +1,3 @@
 export { default as TrackList } from '@/components/account/Playlist/List'
 export { default as Track } from '@/components/account/Playlist/Track'
+export { default as TrackPresent } from '@/components/account/Playlist/TrackPresent'
