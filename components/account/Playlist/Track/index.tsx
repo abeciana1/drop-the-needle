13e2a09921ea9726ff0b12a58e3dc593eb95cd59
Alex <alex.beciana@gmail.com>
2023-07-24 15:08:33 -0400

    Removed details tag and summmary child

--- components/account/Playlist/Track/index.tsx
+++ components/account/Playlist/Track/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import { TrackI } from '@/interfaces'
 
 const Track = ({ song, user }: TrackI) => {
+    const [ open, setOpen ] = useState(false)
 
     const {
         title,
@@ -9,21 +11,27 @@ const Track = ({ song, user }: TrackI) => {
         endTime
     } = song
 
+    const toggleOpenDetails = () => {
+        setOpen(!open)
+    }
+
     return(
-        <li className="px-5 py-5">
-            <details>
-                <summary>
-                    <span className='font-bold'>&quot;{title}&quot;</span>
-                    <span> - </span>
-                    <span className='italic'>{artist} (album, year)</span>
-                    <span className='float-right'>{user}</span>
-                </summary>
+        <li onClick={toggleOpenDetails} className="px-5 py-5 cursor-pointer">
+            <div>
+                <span className='font-bold'>&quot;{title}&quot;</span>
+                <span> - </span>
+                <span className='italic'>{artist} (album, year)</span>
+                <span className='float-right'>{user}</span>
+            </div>
+            {open &&
                 <div>
-                    <div className='font-bold'>Timestamps:</div>
-                    <div><span className='font-medium'>Start: </span>{startTime}</div>
-                    <div><span className='font-medium'>End: </span>{endTime}</div>
+                    <div>
+                        <div className='font-bold'>Timestamps:</div>
+                        <div><span className='font-medium'>Start: </span>{startTime}</div>
+                        <div><span className='font-medium'>End: </span>{endTime}</div>
+                    </div>
                 </div>
-            </details>
+                }
         </li>
     )
 }
