07d66d9923f4a1b2bfb835939c9de4cbf30df32c
Alex <alex.beciana@gmail.com>
2023-07-24 20:02:40 -0400

    Added divider lines to tracklist comp

--- components/account/Playlist/List/index.tsx
+++ components/account/Playlist/List/index.tsx
@@ -9,7 +9,7 @@ const TrackList = ({
         <section>
             <div className="font-medium">(Click on the track to expand details)</div>
             {songs?.length > 0 &&
-                <ul className="border-altBlack border-4 rounded-lg">
+                <ul className="border-altBlack border-4 rounded-lg divide-y-4 divide-altBlack">
                     {songs?.map((song: SongI) => {
                         return <Track key={song?.id} song={song} user={song?.participant?.user?.name} />
                     })}
