4dff6c716dbf89abde6312f396bb02989b5429a8
Alex <alex.beciana@gmail.com>
2023-07-24 22:30:23 -0400

    Installed modal types && created styled render of modal to test

--- components/common/Modal/index.tsx
+++ components/common/Modal/index.tsx
@@ -1,8 +1,42 @@
+import React, { useState } from 'react'
+import { OnClickButton } from '@/components/common'
+import { ModalI } from '@/interfaces'
 
-const Modal = () => {
+const Modal: React.FC<any> = ({
+    text,
+    bgColor,
+    onClick,
+    children
+}: ModalI) => {
+    const [ isOpen, setIsOpen ] = useState(false)
+
+    const customStyles = {
+        overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.6)'
+        },
+        content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+        }
+    }
+
 
     return (
         <>
+            <OnClickButton
+                text={text}
+                bgColor={bgColor}
+                onClick={onClick}
+                ctaArrow={false}
+            />
+            <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={customStyles}>
+                <button onClick={() => setIsOpen(false)}>Close Modal</button>
+                { children }
+            </Modal>
         </>
     )
 }
