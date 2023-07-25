b9e0f1b5918c1a151e2898d6370107cef206ddb8
Alex <alex.beciana@gmail.com>
2023-07-24 20:50:13 -0400

    Added hover toggle remove remove button to render

--- components/account/Playlist/Track/index.tsx
+++ components/account/Playlist/Track/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import { TrackI } from '@/interfaces'
 import { TrackPresent } from '@/components/account'
+import { OnClickButton } from '@/components/common'
 
 const Track = ({ song, user }: TrackI) => {
     const [ open, setOpen ] = useState(false)
+    const [ hover, setHover ] = useState(false)
 
     const {
         title,
@@ -17,18 +19,42 @@ const Track = ({ song, user }: TrackI) => {
         setOpen(!open)
     }
 
+    const toggleRemoveTrack = () => {
+        console.log('remove')
+    }
 
+    const toggleHover = () => {
+        setHover(!hover)
+    }
 
     return(
-        <li onClick={toggleOpenDetails} className="px-5 py-5 cursor-pointer">
-            <div>
-                <span className='font-bold'>&quot;{title}&quot;</span>
-                <span> - </span>
-                <span className='italic'>{artist} (album, year)</span>
-                <span className='float-right'>{user}</span>
+        <li 
+            onClick={toggleOpenDetails} 
+            className="px-5 py-5 cursor-pointer"
+            onMouseEnter={toggleHover}
+            onMouseLeave={toggleHover}
+        >
+            <div className='flex flex-wrap items-center justify-between'>
+                <div>
+                    <span className='font-bold'>&quot;{title}&quot;</span>
+                    <span> - </span>
+                    <span className='italic'>{artist} (album, year)</span>
+                </div>
+                {hover ?
+                    <span>
+                        <OnClickButton
+                            text="Remove"
+                            bgColor='vermillion'
+                            ctaArrow={false}
+                            onClick={toggleRemoveTrack}
+                        />
+                    </span>
+                        :
+                    <span className='float-right'>{user}</span>
+                }
             </div>
             {open &&
-                <section className='flex justify-between py-5'>
+                <section className='flex flex-col md:flex-row justify-between py-5'>
                     <div>
                         <div className='font-bold'>Timestamps:</div>
                         <div><span className='font-medium'>Start: </span>{startTime}</div>
