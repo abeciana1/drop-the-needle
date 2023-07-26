1043771e3b4ffeac1ce40d6db2b5dc8be5c61a5f
Alex <alex.beciana@gmail.com>
2023-07-25 21:15:53 -0400

    Exported ModalComp from common comp dir

--- components/common/index.tsx
+++ components/common/index.tsx
@@ -24,4 +24,4 @@ export {
 } from '@/components/common/Button'
 export { default as AccountDD } from '@/components/common/Navigation/AccountDD'
 export { default as PlaylistCard } from '@/components/common/PlaylistCard'
-export { default as Modal } from '@/components/common/Modal'
+export { default as ModalComp } from '@/components/common/Modal'
