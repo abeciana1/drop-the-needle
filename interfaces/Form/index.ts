9a90d1eb96aa424c22a51e4f42fa711ed4a97062
Alex <alex.beciana@gmail.com>
2023-07-25 22:24:57 -0400

    Created FormI interface

--- interfaces/Form/index.ts
+++ interfaces/Form/index.ts
@@ -1,3 +1,5 @@
+import { ChildrenI } from '@/interfaces'
+
 export interface SingleSelectFieldI {
     icon?: React.ElementType;
     labelText: string;
@@ -6,4 +8,8 @@ export interface SingleSelectFieldI {
     selectedValue: any;
     setSelectedValue: (value: any) => void;
     updateFunc?: (value: any) => void;
+}
+
+export interface FormI extends ChildrenI {
+    onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
