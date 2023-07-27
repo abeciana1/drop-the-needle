c9331104d92a313abdba7e31d7995684a3af80a0
Alex <alex.beciana@gmail.com>
2023-07-26 23:45:02 -0400

    Moved onClick={toggleOpenDetails} to <li>

--- components/account/Playlist/Track/index.tsx
+++ components/account/Playlist/Track/index.tsx
@@ -45,6 +45,7 @@ const Track = ({ song, user }: TrackI) => {
 
     return(
         <li
+            onClick={toggleOpenDetails}
             className="px-5 py-5 cursor-pointer"
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
@@ -52,9 +53,7 @@ const Track = ({ song, user }: TrackI) => {
             <div 
                 className='flex flex-wrap items-center justify-between'
             >
-                <div 
-                    onClick={toggleOpenDetails}
-                >
+                <div>
                     <span className='font-bold'>&quot;{trackObj?.trackTitle}&quot;</span>
                     <span> - </span>
                     <span className='italic'>{trackObj?.trackArtist} ({trackObj?.trackAlbum}, {trackObj?.trackYear})</span>
