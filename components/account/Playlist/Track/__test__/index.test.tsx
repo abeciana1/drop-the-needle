2963144289d68d2fcd378968b81d3be09e5e7e0a
Alex <alex.beciana@gmail.com>
2023-08-03 15:48:37 -0400

    Added rendered comp and data props

--- components/account/Playlist/Track/__test__/index.test.tsx
+++ components/account/Playlist/Track/__test__/index.test.tsx
@@ -1,8 +1,31 @@
 import { render, screen } from '@testing-library/react'
 import { Track } from '@/components/account'
 
+const renderedComponent = () => {
+    render(
+        <Track
+            song={
+                {
+                    "id": 1,
+                    "title": "Another One Bites the Dust",
+                    "artist": "The Beatles",
+                    "youtubeLink": "https://www.youtube.com/watch?v=QGnkTQikhsE",
+                    "startTime": "0:10",
+                    "endTime": "0:15",
+                    "orderNumber": 0,
+                    "createdAt": "2023-04-21T18:09:07.793Z",
+                    "participant": {
+                        "name": "Alex"
+                    }
+                }
+            }
+            user='Alex'
+        />
+    )
+}
+
 describe('Track', () => {
-    test('> renders successfully', () => {
+    test.only('> renders successfully', () => {
         
     })
 })
