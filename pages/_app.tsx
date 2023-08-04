ff20f5971c98db07604b1675acfc631e5a134b89
Alex <alex.beciana@gmail.com>
2023-08-04 12:41:00 -0500

    Imported expand-button.css file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
-// import Modal from 'react-modal'
+import '@/styles/expand-button.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   
