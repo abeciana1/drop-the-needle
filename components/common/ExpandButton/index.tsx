22ec00aa45ca44e36267d8bc414f2beedaacdac3
Alex <alex.beciana@gmail.com>
2023-08-05 09:45:44 -0500

    Moved focus state style to conditional classnames list

--- components/common/ExpandButton/index.tsx
+++ components/common/ExpandButton/index.tsx
@@ -21,11 +21,11 @@ export const ExpandBtn = ({
         <button
             style={ open ? {width: `${size}rem`} : {}}
             onClick={onClick}
-            className={cx('py-2 hover:px-4 focus:px-4 blur:px-0 flex overflow-hidden expand-btn rounded-full items-center', {
+            className={cx('py-2 hover:px-4 flex overflow-hidden expand-btn rounded-full items-center', {
                 ['bg-ceruleanBlue text-altWhite']: backgroundColor === 'ceruleanBlue',
                 ['bg-vermillion text-altWhite']: backgroundColor === 'vermillion',
                 ['bg-altGreen-400 text-altWhite']: backgroundColor === 'bg-altGreen-400',
-                ['rounded-lg']: open
+                ['rounded-lg focus:px-4']: open
             })}
             onMouseEnter={() => setClose(true)}
             onMouseLeave={() => setClose(false)}
@@ -69,12 +69,12 @@ export const ShareBtn = ({
             <button
                 style={ open ? {width: `${size}rem`} : {}}
                 onClick={onClick}
-                className={cx('py-2 hover:px-4 focus:px-4 blur:px-0 flex overflow-hidden expand-btn rounded-full items-center', {
+                className={cx('py-2 hover:px-4 flex overflow-hidden expand-btn rounded-full items-center', {
                     ['bg-ceruleanBlue text-altWhite']: backgroundColor === 'ceruleanBlue',
                     ['bg-vermillion text-altWhite']: backgroundColor === 'vermillion',
                     ['bg-yellow-300 text-altBlack']: backgroundColor === 'yellow',
                     ['bg-altGreen-400 text-altWhite']: backgroundColor === 'bg-altGreen-400',
-                    ['rounded-lg']: open
+                    ['rounded-lg focus:px-4']: open
                 })}
                 onMouseEnter={() => setClose(true)}
                 onMouseLeave={() => setClose(false)}
@@ -94,12 +94,12 @@ export const ShareBtn = ({
             <a
                 style={ open ? {width: `${size}rem`} : {}}
                 href={sms ? smsHref : emailHref}
-                className={cx('py-2 hover:px-4 focus:px-4 blur:px-0 flex overflow-hidden expand-btn rounded-full items-center', {
+                className={cx('py-2 hover:px-4 flex overflow-hidden expand-btn rounded-full items-center', {
                     ['bg-ceruleanBlue text-altWhite']: backgroundColor === 'ceruleanBlue',
                     ['bg-vermillion text-altWhite']: backgroundColor === 'vermillion',
                     ['bg-yellow-300 text-altBlack']: backgroundColor === 'yellow',
                     ['bg-altGreen-400 text-altWhite']: backgroundColor === 'bg-altGreen-400',
-                    ['rounded-lg']: open
+                    ['rounded-lg focus:px-4']: open
                 })}
                 onMouseEnter={() => setClose(true)}
                 onMouseLeave={() => setClose(false)}
