cbcfbd417a2b4ab4dc88a0648b7b20f1a475f2bc
Alex <alex.beciana@gmail.com>
2023-08-06 12:33:20 -0500

    Added dnd props and comps && Added drag handling func

--- components/account/Playlist/List/index.tsx
+++ components/account/Playlist/List/index.tsx
@@ -1,3 +1,4 @@
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { TrackListI, SongI } from '@/interfaces'
 import { Track } from '@/components/account'
 
@@ -5,16 +6,54 @@ const TrackList = ({
     songs
 }: TrackListI) => {
 
+    const handleOnDragEnd = (result: any) => {
+        console.log({result})
+        if (!result.destination) return;
+        let items = songs
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+        // setSongs(items);
+        // reorderSongs(playlist.playlist.id, parseInt(result.draggableId), result.destination.index)
+    }
+
     return(
         <section>
             <div className="font-medium">(Click on the track to expand details)</div>
-            {songs?.length > 0 &&
-                <ul className="border-altBlack border-4 rounded-lg divide-y-4 divide-altBlack">
-                    {songs?.map((song: SongI) => {
-                        return <Track key={song?.id} song={song} user={song?.participant?.user?.name} />
-                    })}
-                </ul>
-            }
+            <DragDropContext
+                onDragEnd={(result) => handleOnDragEnd(result)}
+            >
+                {songs?.length > 0 &&
+                    <Droppable droppableId="droppable-list">
+                        {(provided) => (
+                            <ul
+                                className="border-altBlack border-4 rounded-lg divide-y-4 divide-altBlack"
+                                ref={provided.innerRef}
+                                {...provided.droppableProps}
+                            >
+                                {songs?.map((song: SongI, index: number) => {
+                                    return (
+                                        <Draggable
+                                            key={song?.id}
+                                            draggableId={song?.id.toString()}
+                                            index={index}
+                                        >
+                                            {(provided) => (
+                                                <Track
+                                                    song={song}
+                                                    user={song?.participant?.user?.name} 
+                                                    provided={provided}
+                                                    index={index}
+                                                />
+                                            )}
+                                        </Draggable>
+                                    )
+                                })}
+                                {provided.placeholder}
+                            </ul>
+                        )}
+                    </Droppable>
+                }
+            </DragDropContext>
         </section>
     )
 }
