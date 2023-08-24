49790a67828044f6d1ce42926e174f6d078dddb5
Alex <alex.beciana@gmail.com>
2023-08-24 18:25:37 -0400

    Exported TextFormatInput from form inputs root

--- components/account/Form/Inputs/index.tsx
+++ components/account/Form/Inputs/index.tsx
@@ -1,2 +1,3 @@
 export { TextInput } from '@/components/account/Form/Inputs/TextInput'
 export { LenLimitInput } from '@/components/account/Form/Inputs/LenLimitInput'
+export { TextFormatInput } from '@/components/account/Form/Inputs/TextFormatInput'
