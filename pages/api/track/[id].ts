49a3a100c832b9bfeb7856b43390564f6f6fbd54
Alex <alex.beciana@gmail.com>
2023-08-29 21:11:07 -0400

    Added case for delete method

--- pages/api/track/[id].ts
+++ pages/api/track/[id].ts
@@ -3,12 +3,18 @@ import prisma from '@/hooks/prisma'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'PATCH') {
-        let trackId = Number(req?.query?.id)
         let track = await prisma?.powerHourSong?.update({
             where: {
-                id: trackId
+                id: Number(req?.query?.id)
             },
             data: req.body
+        })
+        res.status(200).json({ track })
+    } else if (req.method === 'DELETE') {
+        let track = await prisma.powerHourSong.delete({
+            where: {
+                id: Number(req?.query?.id)
+            }
         })
         res.status(200).json({ track })
     }
