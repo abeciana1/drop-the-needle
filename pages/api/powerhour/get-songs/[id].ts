54fd2ae56580bcd85d8164a5b819f91b67e5812b
Alex <alex.beciana@gmail.com>
2023-09-04 18:05:56 -0400

    Deleted get-songs endpoint replaced with new file in new dir

---
+++ pages/api/powerhour/get-songs/[id].ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/hooks/prisma'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'GET') {
+        const powerHourSongs = await prisma?.powerHour?.findFirst({
+            where: {
+                id: Number(req?.query?.id)
+            },
+            select: {
+                PowerHourSongs: true
+            }
+        })
+        let sortedSongs = powerHourSongs?.PowerHourSongs.sort((a, b) => a.orderNumber - b.orderNumber)
+        res.status(200).json({ sortedSongs })
+    }
+}
+
+export default handler
