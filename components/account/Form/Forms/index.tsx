d070399e45118bad9015fa5657d056eea2728fd0
Alex <alex.beciana@gmail.com>
2023-09-05 19:00:28 -0400

    Exported UpdatePowerHourForm from account comps forms dir

--- components/account/Form/Forms/index.tsx
+++ components/account/Form/Forms/index.tsx
@@ -1,1 +1,2 @@
 export { default as UpdateTrackForm } from '@/components/account/Form/Forms/UpdateTrackForm'
+export { default as UpdatePowerHourForm} from '@/components/account/Form/Forms/UpdatePowerHourForm'
