f3c29b96ec70bf07b7a746c0090468e8da323ddf
Alex <alex.beciana@gmail.com>
2023-09-05 18:58:05 -0400

    Created UpdatePowerHourFormI

--- interfaces/Form/Forms/index.ts
+++ interfaces/Form/Forms/index.ts
@@ -8,3 +8,14 @@ export interface UpdateTrackFormI {
     youtubeLink: string;
     submitHandler: (e: React.FormEvent<HTMLFormElement>, data: any) => void;
 }
+
+export interface UpdatePowerHourFormI {
+    title: string;
+    description: string;
+    coverImage: string;
+    dateTime: Date;
+    privateStatus: boolean;
+    publishStatus: boolean;
+    songLimit: number;
+    submitHandler: (e: React.FormEvent<HTMLFormElement>, data: any) => void;
+}
