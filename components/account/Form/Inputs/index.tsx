c02d2073474d1518a899e04daac504809162aed9
Alex <alex.beciana@gmail.com>
2023-09-11 22:22:47 -0400

    Removed old exports

--- components/account/Form/Inputs/index.tsx
+++ components/account/Form/Inputs/index.tsx
@@ -1,3 +1,1 @@
 export { Input } from '@/components/account/Form/Inputs/Input'
-export { LenLimitInput } from '@/components/account/Form/Inputs/LenLimitInput'
-export { TextFormatInput } from '@/components/account/Form/Inputs/TextFormatInput'
