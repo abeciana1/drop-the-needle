f9dc7d6f168b54c4aece945ca59b45c9921bcf43
Alex <alex.beciana@gmail.com>
2023-09-11 22:22:37 -0400

    Added  inputs and useForm hook for form field and error messages

--- components/account/Form/Forms/UpdateTrackForm/index.tsx
+++ components/account/Form/Forms/UpdateTrackForm/index.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
+import { useForm } from "react-hook-form"
 import {
     ModalComp,
     SubmitButton
 } from '@/components/common'
 import {
-    FormContainer
+    FormContainer,
+    Input
 } from '@/components/account'
 import { UpdateTrackFormI } from '@/interfaces'
+import { ErrorMessage } from "@hookform/error-message"
 
 const UpdateTrackForm = ({
     title,
@@ -18,50 +21,125 @@ const UpdateTrackForm = ({
     youtubeLink,
     submitHandler
 }: UpdateTrackFormI) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
     const [ edit, setEdit ] = useState(false)
 
-    const [ songObj, setSongObj ] = useState({
-        songTitle: title,
-        songArtist: artist,
-        songStartTime: startTime,
-        songEndTime: endTime,
-        songAlbum: album,
-        songYear: year,
-        songLink: youtubeLink
-    })
-    
-    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        event.persist()
-        setSongObj({
-            ...songObj,
-            [event.target.name]: event.target.value
-        })
-    }
-    
-    const toggleEditForm = (event: React.FormEvent<HTMLFormElement>) => {
-        event.stopPropagation()
-        event.preventDefault()
+    const submitTest = (data: any) => {
         setEdit(!edit)
-        submitHandler(event, songObj)
+        submitHandler(data)
     }
     
     return(
     <ModalComp
         shouldCloseOnOverlayClick={false}
         shouldCloseOnEsc={false}
-        onClick={toggleEditForm}
+        onClick={() => setEdit(true)}
         render={edit}
         setRender={setEdit}
         text="Edit"
         bgColor='ceruleanBlue'
     >
         <FormContainer
-            onSubmit={toggleEditForm}
+            onSubmit={handleSubmit(submitTest)}
         >
+            <Input
+                label='Title'
+                name='title'
+                fieldRequired='This field is required.'
+                register={register}
+                registerOptions={{
+                    value: title
+                }}
+            />
+            <ErrorMessage name='title' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+                label='Artist'
+                name='artist'
+                fieldRequired='This field is required.'
+                register={register}
+                registerOptions={{
+                    value: artist
+                }}
+            />
+            <ErrorMessage name='artist' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+                label='Album'
+                name='album'
+                fieldRequired='This field is required.'
+                register={register}
+                registerOptions={{
+                    value: album
+                }}
+            />
+            <ErrorMessage name='album' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+                label='Year'
+                name='year'
+                type='number'
+                fieldRequired='This field is required.'
+                register={register}
+                registerOptions={{
+                    value: year,
+                    minLength: {
+                        value: 4,
+                        message: 'This field requires a minimum of 4 digits.'
+                    },
+                    maxLength: {
+                        value: 4,
+                        message: 'This field may not exceed more than 4 digits.'
+                    }
+                }}
+            />
+            <ErrorMessage name='year' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+                label='YouTube link'
+                name='youtubeLink'
+                fieldRequired='This field is required.'
+                register={register}
+                registerOptions={{
+                    value: youtubeLink,
+                    pattern: {
+                        value: new RegExp("youtube.com/watch\\?v=", "gi"),
+                        message: "This field requires a pattern like — https://www.youtube.com/watch?v=QGnkTQikhsE"
+                    }
+                }}
+            />
+            <ErrorMessage name='youtubeLink' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+                label='Start time'
+                name='startTime'
+                fieldRequired='This field is required.'
+                register={register}
+                registerOptions={{
+                    value: startTime,
+                    pattern: {
+                        value: /^[0-9]:[0-5][0-9]$/gi,
+                        message: "This field requires a pattern like — 1:55"
+                    }
+                }}
+            />
+            <ErrorMessage name='startTime' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+                label='End time'
+                name='endTime'
+                fieldRequired='This field is required.'
+                register={register}
+                registerOptions={{
+                    value: endTime,
+                    pattern: {
+                        value: /^[0-9]:[0-5][0-9]$/gi,
+                        message: "This field requires a pattern like — 1:55"
+                    }
+                }}
+            />
+            <ErrorMessage name='endTime' errors={errors} as='div' className='text-vermillion'/>
             <div className="py-3">
                 <SubmitButton
                     bgColor='vermillion'
-                    // disabled={errorsPresent !== 0}
                 />
             </div>
         </FormContainer>
