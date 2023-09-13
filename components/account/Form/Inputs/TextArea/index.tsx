b4c6c6869d55acf92c9c174bf36a70ab10bfd82e
Alex <alex.beciana@gmail.com>
2023-09-12 21:39:58 -0400

    Created TextArea comp && Added renderings and style

---
+++ components/account/Form/Inputs/TextArea/index.tsx
@@ -0,0 +1,27 @@
+import { InputType } from '@/types'
+
+const TextArea = ({
+    label,
+    fieldRequired,
+    name,
+    register,
+    registerOptions
+}: InputType) => {
+
+    return (
+        <div className="py-3">
+            <label>{ label }{fieldRequired && <span className='text-vermillion'>*</span>}</label>
+            <textarea
+                {...register(name, {
+                    required: fieldRequired,
+                    ...registerOptions
+                })}
+                className="py-1 pl-4 ring-2 ring-slate-200 rounded-lg focus:ring-royalBlue text-xl w-full"
+            >
+
+            </textarea>
+        </div>
+    )
+}
+
+export default TextArea
