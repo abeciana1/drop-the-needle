a0df58208e071b3064a7f569ee90957eb6c64e24
Alex <alex.beciana@gmail.com>
2023-09-24 13:10:41 -0400

    Added conditional statement for GET and PATCH requests

--- pages/api/powerhour/[id].ts
+++ pages/api/powerhour/[id].ts
@@ -2,24 +2,34 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/hooks/prisma'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const powerHour = await prisma?.powerHour?.findFirst({
-        where: {
-            id: Number(req?.query?.id)
-        },
-        include: {
-            participants: {                
-                select: {
-                    user: {
-                        select: {
-                            id: true,
-                            name: true
+    const powerHourId = Number(req?.query?.id)
+    if (req.method === 'GET') {
+        const powerHour = await prisma?.powerHour?.findFirst({
+            where: {
+                id: powerHourId
+            },
+            include: {
+                participants: {                
+                    select: {
+                        user: {
+                            select: {
+                                id: true,
+                                name: true
+                            }
                         }
                     }
                 }
             }
-        }
-    })
-    res.status(200).json({ powerHour })
+        })
+        res.status(200).json({ powerHour })
+    }
+    if (req.method === 'PATCH') {
+        const powerHour = await prisma?.powerHour?.update({
+            where: { id: powerHourId },
+            data: req.body
+        })
+        res.status(200).json({ powerHour })
+    }
 }
 
 export default handler
