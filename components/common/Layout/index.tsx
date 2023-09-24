d7e829f680fa3e43d0314299ef3165ded34bc185
Alex <alex.beciana@gmail.com>
2023-09-24 17:43:12 -0400

    Updated padding

--- components/common/Layout/index.tsx
+++ components/common/Layout/index.tsx
@@ -17,7 +17,7 @@ export const Grid2Column = ({
 
     return (
         <section
-            className="grid grid-cols-1 md:grid-cols-2 gap-10 my-20"
+            className="grid grid-cols-1 md:grid-cols-2 gap-10 py-20"
         >
             {children}
         </section>
