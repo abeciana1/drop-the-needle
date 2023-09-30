f22142f6cbd7d16ff91d279ed3a3dc18e56e7fb2
Alex <alex.beciana@gmail.com>
2023-09-29 21:34:16 -0400

    Removed var use

--- pages/api/powerhour/[id].ts
+++ pages/api/powerhour/[id].ts
@@ -2,11 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/hooks/prisma'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const powerHourId = Number(req?.query?.id)
     if (req.method === 'GET') {
         const powerHour = await prisma?.powerHour?.findFirst({
             where: {
-                id: powerHourId
+                id: Number(req?.query?.id)
             },
             include: {
                 participants: {                
@@ -24,14 +23,14 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         res.status(200).json({ powerHour })
     } else if (req.method === 'PATCH') {
         const powerHour = await prisma?.powerHour?.update({
-            where: { id: powerHourId },
+            where: { id: Number(req?.query?.id) },
             data: req.body
         })
         res.status(200).json({ powerHour })
     }  else if (req.method === 'DELETE') {
         const powerHour = await prisma?.powerHour?.delete({
             where: {
-                id: powerHourId
+                id: Number(req?.query?.id)
             }
         })
         res.status(200).json({ powerHour })
