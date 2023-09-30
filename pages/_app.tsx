612a318bf5b47bc4fec142c62e781634e06a56bb
Alex <alex.beciana@gmail.com>
2023-09-30 10:55:37 -0400

    Added LoadingStore context comp and LoadingInstance comp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,17 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import '@/styles/expand-button.css'
+import LoadingStore from '@/context/loading-context'
+import { LoadingInstance } from '@/components/common'
 
 export default function App({ Component, pageProps }: AppProps) {
   
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <LoadingStore>
+        <LoadingInstance />
+        <Component {...pageProps} />
+      </LoadingStore>
     </SessionProvider>
   )
 }
