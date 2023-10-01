b1a71b60c593e15db316de94c24652bb7faa5c96
Alex <alex.beciana@gmail.com>
2023-09-30 21:22:47 -0400

    Changed import names for store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,16 @@ import '@/styles/expand-button.css'
 import '@/styles/loading-animation.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
-import LoadingStore from '@/context/loading-context'
+import ModalInstanceStore from '@/context/instance-context'
 import { LoadingInstance } from '@/components/common'
 
 export default function App({ Component, pageProps }: AppProps) {
   
   return (
     <SessionProvider session={pageProps.session}>
-      <LoadingStore>
-        <LoadingInstance />
+      <ModalInstanceStore>
         <Component {...pageProps} />
-      </LoadingStore>
+      </ModalInstanceStore>
     </SessionProvider>
   )
 }
