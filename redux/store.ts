63bacfba77c88b51692dc22ec844682e675947db
Alex <alex.beciana@gmail.com>
2023-10-03 21:56:53 -0400

    Configured store  and set type for dispatch and selector

---
+++ redux/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit'
+import loadingReducer from '@/redux/slices/loadingSlice'
+import instanceReducer from '@/redux/slices/instanceSlice'
+
+const store =  configureStore({
+    reducer: {
+        loading: loadingReducer,
+        instance: instanceReducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+    devTools: process.env.NODE_ENV !== 'production'
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store
