37dbe1786e5b4fb71ebff90b6b000ff53a76b6b6
Alex <alex.beciana@gmail.com>
2023-10-05 17:37:35 -0400

    Removed redundant code

--- components/account/Form/Forms/AddTrackForm/index.tsx
+++ components/account/Form/Forms/AddTrackForm/index.tsx
@@ -13,7 +13,6 @@ import { ErrorMessage } from "@hookform/error-message"
 import { useSession } from 'next-auth/react'
 
 const AddTrackForm = ({
-    icon,
     submitHandler
 }: AddTrackFormI) => {
     const {
@@ -21,7 +20,6 @@ const AddTrackForm = ({
         handleSubmit,
         formState: { errors },
     } = useForm()
-    const [ edit, setEdit ] = useState(false)
     const { data: session } = useSession()
 
 
@@ -33,107 +31,95 @@ const AddTrackForm = ({
                 powerHourId: Number(phId),
                 participantId: session?.user?.id
             })
-            setEdit(!edit)
         }
     }
 
     return (
-        <ModalComp
-            shouldCloseOnOverlayClick={false}
-            shouldCloseOnEsc={false}
-            onClick={() => setEdit(true)}
-            render={edit}
-            setRender={setEdit}
-            text="Add a track"
-            bgColor='ceruleanBlue'
-            icon={icon}
-        >
-            <FormContainer onSubmit={handleSubmit(submit)}>
-                <Input
-                    label='Title'
-                    name='title'
-                    fieldRequired='This field is required.'
-                    register={register}
-                />
-                <ErrorMessage name='title' errors={errors} as='div' className='text-vermillion'/>
-                <Input
-                label='Artist'
-                name='artist'
+        <FormContainer onSubmit={handleSubmit(submit)}>
+            <Input
+                label='Title'
+                name='title'
                 fieldRequired='This field is required.'
                 register={register}
+            />
+            <ErrorMessage name='title' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+            label='Artist'
+            name='artist'
+            fieldRequired='This field is required.'
+            register={register}
+            />
+            <ErrorMessage name='artist' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+                label='Album'
+                name='album'
+                fieldRequired='This field is required.'
+                register={register}
+            />
+            <ErrorMessage name='album' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+                label='Year'
+                name='year'
+                type='number'
+                fieldRequired='This field is required.'
+                register={register}
+                registerOptions={{
+                    minLength: {
+                        value: 4,
+                        message: 'This field requires a minimum of 4 digits.'
+                    },
+                    maxLength: {
+                        value: 4,
+                        message: 'This field may not exceed more than 4 digits.'
+                    }
+                }}
+            />
+            <ErrorMessage name='year' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+                label='YouTube link'
+                name='youtubeLink'
+                fieldRequired='This field is required.'
+                register={register}
+                registerOptions={{
+                    pattern: {
+                        value: new RegExp("youtube.com/watch\\?v=", "gi"),
+                        message: "This field requires a pattern like — https://www.youtube.com/watch?v=QGnkTQikhsE"
+                    }
+                }}
+            />
+            <ErrorMessage name='youtubeLink' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+                label='Start time'
+                name='startTime'
+                fieldRequired='This field is required.'
+                register={register}
+                registerOptions={{
+                    pattern: {
+                        value: /^[0-9]:[0-5][0-9]$/gi,
+                        message: "This field requires a pattern like — 1:55"
+                    }
+                }}
+            />
+            <ErrorMessage name='startTime' errors={errors} as='div' className='text-vermillion'/>
+            <Input
+                label='End time'
+                name='endTime'
+                fieldRequired='This field is required.'
+                register={register}
+                registerOptions={{
+                    pattern: {
+                        value: /^[0-9]:[0-5][0-9]$/gi,
+                        message: "This field requires a pattern like — 1:55"
+                    }
+                }}
+            />
+            <ErrorMessage name='endTime' errors={errors} as='div' className='text-vermillion'/>
+            <div className="py-3">
+                <SubmitButton
+                    bgColor='vermillion'
                 />
-                <ErrorMessage name='artist' errors={errors} as='div' className='text-vermillion'/>
-                <Input
-                    label='Album'
-                    name='album'
-                    fieldRequired='This field is required.'
-                    register={register}
-                />
-                <ErrorMessage name='album' errors={errors} as='div' className='text-vermillion'/>
-                <Input
-                    label='Year'
-                    name='year'
-                    type='number'
-                    fieldRequired='This field is required.'
-                    register={register}
-                    registerOptions={{
-                        minLength: {
-                            value: 4,
-                            message: 'This field requires a minimum of 4 digits.'
-                        },
-                        maxLength: {
-                            value: 4,
-                            message: 'This field may not exceed more than 4 digits.'
-                        }
-                    }}
-                />
-                <ErrorMessage name='year' errors={errors} as='div' className='text-vermillion'/>
-                <Input
-                    label='YouTube link'
-                    name='youtubeLink'
-                    fieldRequired='This field is required.'
-                    register={register}
-                    registerOptions={{
-                        pattern: {
-                            value: new RegExp("youtube.com/watch\\?v=", "gi"),
-                            message: "This field requires a pattern like — https://www.youtube.com/watch?v=QGnkTQikhsE"
-                        }
-                    }}
-                />
-                <ErrorMessage name='youtubeLink' errors={errors} as='div' className='text-vermillion'/>
-                <Input
-                    label='Start time'
-                    name='startTime'
-                    fieldRequired='This field is required.'
-                    register={register}
-                    registerOptions={{
-                        pattern: {
-                            value: /^[0-9]:[0-5][0-9]$/gi,
-                            message: "This field requires a pattern like — 1:55"
-                        }
-                    }}
-                />
-                <ErrorMessage name='startTime' errors={errors} as='div' className='text-vermillion'/>
-                <Input
-                    label='End time'
-                    name='endTime'
-                    fieldRequired='This field is required.'
-                    register={register}
-                    registerOptions={{
-                        pattern: {
-                            value: /^[0-9]:[0-5][0-9]$/gi,
-                            message: "This field requires a pattern like — 1:55"
-                        }
-                    }}
-                />
-                <ErrorMessage name='endTime' errors={errors} as='div' className='text-vermillion'/>
-                <div className="py-3">
-                    <SubmitButton
-                        bgColor='vermillion'
-                    />
-                </div>
-            </FormContainer>
-        </ModalComp>
+            </div>
+        </FormContainer>
     )
 }
 
