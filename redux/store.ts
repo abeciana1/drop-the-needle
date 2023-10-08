1f6572b53d89b4a2fcfcbf9b1b1d597d026b22b8
Alex <alex.beciana@gmail.com>
2023-10-08 08:24:31 -0400

    Removed unused import

--- redux/store.ts
+++ redux/store.ts
@@ -3,21 +3,20 @@ import thunk from 'redux-thunk'
 import loadingReducer from '@/redux/slices/loadingSlice'
 import instanceReducer from '@/redux/slices/instanceSlice'
 import powerHourReducer from '@/redux/slices/powerHourSlice'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
 const middlewareEnhancer = [thunk]
 
 const store =  configureStore({
-    reducer: {
-        loading: loadingReducer,
-        instance: instanceReducer,
-        powerHour: powerHourReducer
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: false,
-    }).concat(middlewareEnhancer),
-    devTools: process.env.NODE_ENV !== 'production'
-})
+        reducer: {
+            loading: loadingReducer,
+            instance: instanceReducer,
+            powerHour: powerHourReducer
+        },
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+            serializableCheck: false,
+        }).concat(middlewareEnhancer),
+        devTools: process.env.NODE_ENV !== 'production'
+    })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
