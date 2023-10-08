0263c53e399b029126a833168b69c766cf05729b
Alex <alex.beciana@gmail.com>
2023-10-08 09:13:03 -0400

    Imported patchPowerHour dispatch && Created and exported updatePowerHour action && Fixed error with songs rendering

--- redux/actions/playlist-actions.ts
+++ redux/actions/playlist-actions.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import {
     setPowerHour,
-    setSongs
+    setSongs,
+    patchPowerHour
 } from '@/redux/slices/powerHourSlice'
 import {
     loading,
@@ -33,11 +34,34 @@ export const fetchSongs = (id: string) => {
             await axios.get('/api/powerhour/get-songs/' + id)
             .then(res => {
                 dispatch(success())
-                dispatch(setSongs(res.data.powerHour))
+                dispatch(setSongs(res.data.sortedSongs))
             })
         } catch (error) {
             dispatch(failure({ error: 'Failed to fetch power hour data' }))
             console.error('fetchPowerHour', error);
+        }
+    }
+}
+
+export const updatePowerHour = (id: number, data: any) => {
+    return async function (dispatch: AppDispatch) {
+        dispatch(loading())
+        try {
+            await axios.patch(`/api/powerhour/${id}`, {
+                title: data?.title,
+                description: data?.description,
+                date_time: new Date(data?.dateTime),
+                privateStatus: data?.privateStatus === 'true',
+                publishStatus: data?.publishStatus === 'true',
+                songLimit: data?.songLimit
+            })
+            .then(res => {
+                dispatch(success())
+                dispatch(patchPowerHour(res.data.powerHour))
+            })
+        } catch (err) {
+            dispatch(failure({ error: 'Failed to update power hour data' }))
+            console.log(err)
         }
     }
 }
