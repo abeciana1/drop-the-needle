7dc576555d9a92d6c5bffe2ab26d85d50a5e822c
Alex <alex.beciana@gmail.com>
2023-10-24 21:54:49 -0400

    Removed space && Added hostedLink prop && Added vars for hrefs for different use with comp

--- components/common/PlaylistCard/index.tsx
+++ components/common/PlaylistCard/index.tsx
@@ -9,13 +9,17 @@ const PlaylistCard = ({
     cover_image,
     publicLink = false,
     date,
-    time
+    time,
+    hostedLink = false
 }: DashPowerHourType) => {
+    let href = `/${publicLink ? 'listen' : 'dashboard'}/powerhour/${publicLink ? '' : 'participant/'}${encodeURI(id.toString())}`
+    let hostedHref = `/dashboard/powerhour/hosted/${encodeURI(id.toString())}`
+
     return (
         <Link
-            href={`/${publicLink ? 'listen' : 'dashboard'}/powerhour/${encodeURI(id.toString())}`}
+            href={hostedLink ? hostedHref : href}
             title={title}
-            className="text-xl  font-semiBold"
+            className="text-xl font-semiBold"
         >
             <Image
                 src={cover_image}
