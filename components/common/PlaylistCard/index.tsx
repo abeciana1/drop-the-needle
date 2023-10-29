c1caa5e0ac687b81bf3a47f2ebf7bd2646afe41b
Alex <alex.beciana@gmail.com>
2023-10-28 22:38:00 -0400

    Imported invite card interface and hooks and action && Created invite card component

--- components/common/PlaylistCard/index.tsx
+++ components/common/PlaylistCard/index.tsx
@@ -1,7 +1,11 @@
-import { DashPowerHourType } from "@/types" 
+import { DashPowerHourType } from "@/types"
 import Link from 'next/link'
 import Image from 'next/image'
-import { set } from 'lodash'
+import { InviteCardI } from '@/interfaces'
+import { setInstance } from "@/redux/slices/instanceSlice"
+import {
+    useAppDispatch
+} from '@/redux/hooks'
 
 const PlaylistCard = ({
     id,
@@ -39,3 +43,57 @@ const PlaylistCard = ({
 }
 
 export default PlaylistCard
+
+export const InviteCard = ({
+    id,
+    index,
+    title,
+    description,
+    cover_image,
+    date,
+    time,
+    rsvpYes,
+    rsvpNo,
+    rsvpMaybe
+}: InviteCardI) => {
+    const dispatch = useAppDispatch()
+
+    const renderInviteModal = () => {
+        dispatch(setInstance({
+            display: true,
+            name: 'inviteModal',
+            data: {
+                id: id,
+                index: index,
+                title: title,
+                description: description,
+                date: date,
+                time: time,
+                rsvpYes: rsvpYes,
+                rsvpNo: rsvpNo,
+                rsvpMaybe: rsvpMaybe
+            }
+        }))
+    }
+
+    return (
+        <div
+            onClick={renderInviteModal}
+            className="text-xl font-semiBold"
+        >
+            <Image
+                src={cover_image}
+                priority
+                alt={title}
+                width={225}
+                height={225}
+                className='w-56 h-56 mx-auto'
+            />
+            <div className='text-center'>
+                {title}
+            </div>
+            <div className='text-center'>{date}</div>
+            <div className='text-center'>{time}</div>
+        </div>
+    )
+}
