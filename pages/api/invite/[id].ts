fee346ef6e165e607d4c4dc6aa82ff819fc95bb6
Alex <alex.beciana@gmail.com>
2023-10-28 22:55:44 -0400

    Created api endpoint to handle patch update for invite

---
+++ pages/api/invite/[id].ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/hooks/prisma'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'PATCH') {
+        const invite = await prisma?.invite?.update({
+            where: {
+                id: Number(req?.query?.id)
+            },
+            data: req.body
+        })
+        res.status(200).json({ invite })
+    }
+}
+
+export default handler
