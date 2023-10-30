b50615148ff1e1c1389c43dbc518d523fb433c9f
Alex <alex.beciana@gmail.com>
2023-10-29 20:35:09 -0400

    Created api endpoint to create participant based on invite rsvp

---
+++ pages/api/participant/new.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/hooks/prisma'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST') {
+        const participant = await prisma.participant.create({
+            data: {
+                powerHourId: req?.body?.powerHourId,
+                userId: req?.body?.userId
+            }
+        })
+        res.status(200).json({ participant })
+    }
+}
+
+export default handler
