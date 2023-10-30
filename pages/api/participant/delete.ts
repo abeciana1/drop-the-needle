36c8e6aee22069e5916cb14169663579de62c72a
Alex <alex.beciana@gmail.com>
2023-10-29 20:34:37 -0400

    Created delete api endpoint for participants

---
+++ pages/api/participant/delete.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/hooks/prisma'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST') {
+        const participant = await prisma.participant.deleteMany({
+            where: {
+                powerHourId: req?.body?.powerHourId,
+                userId: req?.body?.userId
+            }
+        })
+        console.log('participant', participant)
+        res.status(200).json({ participant })
+    }
+}
+
+export default handler
