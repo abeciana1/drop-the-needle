10941bba7b0ee7d6a3e68c7e1d11fb2fac5f473b
Alex <alex.beciana@gmail.com>
2023-10-29 20:28:43 -0400

    Created RSVPI interface && Added axios dispatch action to update invite

--- redux/actions/invite-actions.ts
+++ redux/actions/invite-actions.ts
@@ -6,8 +6,27 @@ import {
 } from '@/redux/slices/loadingSlice'
 import { AppDispatch } from '@/redux/store'
 
-export const updateInvitesAction = () => {
+interface RSVPI {
+    rsvpYes: boolean;
+    rsvpNo: boolean;
+    rsvpMaybe: boolean;
+}
+
+export const updateInvitesAction = (inviteId: number, {rsvpYes, rsvpNo, rsvpMaybe}: RSVPI) => {
     return async (dispatch: AppDispatch) => {
         dispatch(loading())
+        try {
+            axios.patch('/api/invite/' + inviteId, {
+                rsvpYes: rsvpYes,
+                rsvpNo: rsvpNo,
+                rsvpMaybe: rsvpMaybe
+            })
+            .then(res => {
+                console.log('res', res)
+                dispatch(success())
+            })
+        } catch (error) {
+            console.error('err', error)
+        }
     }
 }
