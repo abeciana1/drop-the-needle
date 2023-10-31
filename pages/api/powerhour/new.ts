a6d90bf860e7f5f222e0b3c5310a59b77444d923
Alex <alex.beciana@gmail.com>
2023-10-31 16:35:40 -0400

    Removed console log

--- pages/api/powerhour/new.ts
+++ pages/api/powerhour/new.ts
@@ -14,7 +14,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
                 }
             }
         })
-        console.log('newPowerHour', newPowerHour)
         res.status(200).json({ newPowerHour })
     }
 }
