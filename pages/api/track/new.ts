7b05fb827a434acb92bdbc05c996b94c044e1290
Alex <alex.beciana@gmail.com>
2023-10-31 16:34:28 -0400

    Removed unneeded extra code paths in query

--- pages/api/track/new.ts
+++ pages/api/track/new.ts
@@ -3,10 +3,32 @@ import prisma from '@/hooks/prisma'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
-        let newTrack = await prisma.powerHourSong.create({
-            data: req.body
+        let foundParticipant = await prisma.participant.findFirst({
+            where: {
+                userId: req.body.participantId,
+                powerHourId: req.body.powerHourId
+            },
+            select: {
+                id: true
+            }
         })
-        res.status(200).json({ newTrack })
+        if (foundParticipant) {
+            let newTrack = await prisma.powerHourSong.create({
+                data: {
+                    title: req.body.title,
+                    artist: req.body.artist,
+                    youtubeLink: req.body.youtubeLink,
+                    startTime: req.body.startTime,
+                    endTime: req.body.endTime,
+                    album: req.body.album,
+                    year: req.body.year,
+                    orderNumber: req.body.orderNumber,
+                    powerHourId: req.body.powerHourId,
+                    participantId: foundParticipant.id
+                }
+            })
+            res.status(200).json({ newTrack })
+        }
     }
 }
 
--- pages/api/user/get-songs.ts
+++ pages/api/user/get-songs.ts
@@ -3,28 +3,20 @@ import prisma from '@/hooks/prisma'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
-        let userSongs = await prisma.user.findFirst({
+        let userSongs = await prisma.participant.findFirst({
             where: {
-                id: req?.body?.userId
+                powerHourId: req?.body?.phId,
+                userId: req?.body?.userId
             },
             select: {
-                participants: {
-                    where: {
-                        powerHourId: req?.body?.phId,
-                        userId: req?.body?.userId
-                    },
-                    select: {
-                        PowerHourSongs: {
-                            orderBy: {
-                                orderNumber: 'asc'
-                            }
-                        }
-                    },
+                PowerHourSongs: {
+                    orderBy: {
+                        orderNumber: 'asc'
+                    }
                 }
             }
         })
-        let songs = userSongs?.participants[0]?.PowerHourSongs
-        res.status(200).json({ songs })
+        res.status(200).json({ userSongs })
     }
 }
 
