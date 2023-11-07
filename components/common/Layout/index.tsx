86f5aa16cbe38683880f4477d231c183297b4e70
Alex <alex.beciana@gmail.com>
2023-11-06 20:38:57 -0500

    Added extra padding for extra large screens

--- components/common/Layout/index.tsx
+++ components/common/Layout/index.tsx
@@ -88,7 +88,7 @@ export const ComponentMargin = ({
 }: ComponentMarginI) => {
 
     return (
-        <section className={cx('relative py-5 px-5 sm:px-10 lg:px-20', {
+        <section className={cx('relative py-5 px-5 sm:px-10 lg:px-20 xl:px-96', {
             ['bg-vermillion-200 text-altBlack']: bgColor === 'vermillion-200',
             ['bg-jaffa-200 text-altBlack']: bgColor === 'jaffa-200',
             ['bg-ceruleanBlue text-altWhite']: bgColor === 'ceruleanBlue',
