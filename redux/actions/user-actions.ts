5e03d74ffd4dbe27d48f18f9024c59376e48ed4d
Alex <alex.beciana@gmail.com>
2023-11-10 15:16:31 -0500

    Removed comment && Created and exported user power hours fetch action

--- redux/actions/user-actions.ts
+++ redux/actions/user-actions.ts
@@ -6,7 +6,7 @@ import {
 } from '@/redux/slices/loadingSlice'
 import { AppDispatch } from '@/redux/store'
 import { setSongs } from '@/redux/slices/powerHourSlice'
-import { setVideos } from '@/redux/slices/userSlice'
+import { setVideos, setUserPowerHours } from '@/redux/slices/userSlice'
 
 export const fetchUserSongsAction = (userId: number, phId: number) => {
     return async (dispatch: AppDispatch) => {
@@ -45,22 +45,19 @@ export const searchYouTubeAction = (searchTerm: string) => {
     }
 }
 
-// * {
-// *     "id": "AW55J2zE3N4",
-// *     "title": "The Beatles - Now And Then (Official Audio)",
-// *     "link": "https://youtu.be/AW55J2zE3N4",
-// *     "thumbnail": "https://i.ytimg.com/vi/AW55J2zE3N4/hqdefault.jpg",
-// *     "channel": {
-// *         "id": "UCc4K7bAqpdBP8jh1j9XZAww",
-// *         "name": "The Beatles",
-// *         "link": "https://www.youtube.com/channel/UCc4K7bAqpdBP8jh1j9XZAww",
-// *         "handle": null,
-// *         "verified": true,
-// *         "thumbnail": "https://yt3.ggpht.com/ytc/APkrFKZaa3FGlL9nr6YnH8_PtgOmEkTxh9C6r77YA6lkxw=s0?imgmax=0"
-// *     },
-// *     "description": "Now and Then's eventful journey to fruition took place over five decades and is the product of conversations and collaborations ...",
-// *     "views": 6455190,
-// *     "uploaded": "4 days ago",
-// *     "duration": 249,
-// *     "durationString": "4:09"
-// * }
+export const fetchUserPowerHoursAction = (userId: number) => {
+    return async (dispatch: AppDispatch) => {
+        dispatch(loading())
+        try {
+            await axios.post('/api/user/get-powerhours', {
+                userId: userId
+            })
+            .then(response => {
+                dispatch(setUserPowerHours(response.data.userPowerHours.participants))
+                dispatch(success())
+            })
+        } catch (err) {
+            console.error('err', err)
+        }
+    }
+}
