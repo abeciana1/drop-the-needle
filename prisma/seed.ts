7b126c6e2bc11f90a929a32be0ca6baa4ae4d6e0
Alex <alex.beciana@gmail.com>
2023-11-10 16:46:30 -0500

    Reseeded db with new table field for PowerHour table

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
-import { faker } from '@faker-js/faker';
+// import { faker } from '@faker-js/faker';
 
 // * destroy records
-const destroyAllRecords = async () => {
-    // await prisma.user.deleteMany()
-    // await prisma.powerHour.deleteMany()
-    // await prisma.host.deleteMany()
-    // await prisma.participant.deleteMany()
-    await prisma.powerHourSong.deleteMany()
-    // await prisma.invite.deleteMany()
-}
+// const destroyAllRecords = async () => {
+//     // await prisma.user.deleteMany()
+//     await prisma.host.deleteMany()
+//     await prisma.powerHourSong.deleteMany()
+//     await prisma.participant.deleteMany()
+//     await prisma.invite.deleteMany()
+//     // await prisma.powerHour.deleteMany()
+// }
 
-console.log('all records destroyed', destroyAllRecords())
+// console.log('all records destroyed', destroyAllRecords())
 
 // let user = {
 //     name: 'Alex',
@@ -39,163 +39,170 @@ console.log('all records destroyed', destroyAllRecords())
 //     }
 // })
 
-const main = async () => {
-    let songs = [
-        {
-            title: faker.music.songName(),
-            artist: "The Beatles",
-            youtubeLink: 'https://www.youtube.com/watch?v=QGnkTQikhsE',
-            startTime: '0:10',
-            endTime: '0:15',
-            powerHourId: 1,
-            orderNumber: 1,
-            participantId: 1
-        },
-        {
-            title: faker.music.songName(),
-            artist: "The Buggles",
-            youtubeLink: 'https://www.youtube.com/watch?v=W8r-tXRLazs',
-            startTime: '0:10',
-            endTime: '0:15',
-            powerHourId: 1,
-            orderNumber: 2,
-            participantId: 1
-        },
-        {
-            title: faker.music.songName(),
-            artist: "Brand New",
-            youtubeLink: 'https://www.youtube.com/watch?v=qgtkPKZ2OPk',
-            startTime: '0:10',
-            endTime: '0:15',
-            powerHourId: 1,
-            orderNumber: 3,
-            participantId: 1
-        },
-        {
-            title: faker.music.songName(),
-            artist: "random 1",
-            youtubeLink: 'https://www.youtube.com/watch?v=qgtkPKZ2OPk',
-            startTime: '0:10',
-            endTime: '0:15',
-            powerHourId: 1,
-            orderNumber: 0,
-            participantId: 1
-        },
-        {
-            title: faker.music.songName(),
-            artist: "random 2",
-            youtubeLink: 'https://www.youtube.com/watch?v=qgtkPKZ2OPk',
-            startTime: '0:10',
-            endTime: '0:15',
-            powerHourId: 1,
-            orderNumber: 0,
-            participantId: 1
-        },
-        {
-            title: faker.music.songName(),
-            artist: "random 3",
-            youtubeLink: 'https://www.youtube.com/watch?v=qgtkPKZ2OPk',
-            startTime: '0:10',
-            endTime: '0:15',
-            powerHourId: 1,
-            orderNumber: 0,
-            participantId: 1
-        }
-    ]
-    console.log('creating songs')
-    for (let song of songs) {
-        await prisma.powerHourSong.create({
-            data: song
-        })
-    }
-    // console.log('creating power hour')
-    // let powerHourObj1 = await prisma.powerHour.create({
-    //     data: {
-    //         title: 'Power Hour 1',
-    //         description: 'Power Hour 1',
-    //         date_time: new Date,
-    //         cover_image: 'https://res.cloudinary.com/dymmbugh2/image/upload/v1697937069/dtn-image/g2mkvb7takf9pojc4ium.webp',
-    //     }
-    // })
-    // await prisma.host.create({
-    //     data: {
-    //         userId: 2,
-    //         powerHourId: powerHourObj1.id
-    //     }
-    // })
-    // await prisma.participant.create({
-    //     data: {
-    //         userId: 2,
-    //         powerHourId: powerHourObj1.id
-    //     }
-    // })
-    // let powerHourObj2 = await prisma.powerHour.create({
-    //     data: {
-    //         title: 'Power Hour 1',
-    //         description: 'Power Hour 1',
-    //         date_time: new Date,
-    //         cover_image: 'https://res.cloudinary.com/dymmbugh2/image/upload/v1697937069/dtn-image/g2mkvb7takf9pojc4ium.webp',
-    //     }
-    // })
-    // await prisma.host.create({
-    //     data: {
-    //         userId: 2,
-    //         powerHourId: powerHourObj2.id
-    //     }
-    // })
-    // await prisma.participant.create({
-    //     data: {
-    //         userId: 2,
-    //         powerHourId: powerHourObj2.id
-    //     }
-    // })
-    // let powerHourObj3 = await prisma.powerHour.create({
-    //     data: {
-    //         title: 'Power Hour 1',
-    //         description: 'Power Hour 1',
-    //         date_time: new Date,
-    //         cover_image: 'https://res.cloudinary.com/dymmbugh2/image/upload/v1697937069/dtn-image/g2mkvb7takf9pojc4ium.webp',
-    //     }
-    // })
-    // await prisma.host.create({
-    //     data: {
-    //         userId: 2,
-    //         powerHourId: powerHourObj3.id
-    //     }
-    // })
-    // await prisma.participant.create({
-    //     data: {
-    //         userId: 2,
-    //         powerHourId: powerHourObj3.id
-    //     }
-    // })
-    // console.log('create invites')
-    // await prisma.invite.create({
-    //     data: {
-    //         userId: 1,
-    //         powerHourId: powerHourObj1.id
-    //     }
-    // })
-    // await prisma.invite.create({
-    //     data: {
-    //         userId: 1,
-    //         powerHourId: powerHourObj2.id
-    //     }
-    // })
-    // await prisma.invite.create({
-    //     data: {
-    //         userId: 1,
-    //         powerHourId: powerHourObj3.id
-    //     }
-    // })
-}
+// const main = async () => {
+//     console.log('creating power hour')
+//     let powerHourObj1 = await prisma.powerHour.create({
+//         data: {
+//             title: 'Power Hour 1',
+//             description: 'Power Hour 1',
+//             date_time: new Date('12-12-2023'),
+//             cover_image: 'https://res.cloudinary.com/dymmbugh2/image/upload/v1697937069/dtn-image/g2mkvb7takf9pojc4ium.webp',
+//             songLimit: 1,
+//             inviteToken: 'k7MMyViZ'
+//         }
+//     })
+//     await prisma.host.create({
+//         data: {
+//             userId: 2,
+//             powerHourId: powerHourObj1.id
+//         }
+//     })
+//     await prisma.participant.create({
+//         data: {
+//             userId: 2,
+//             powerHourId: powerHourObj1.id
+//         }
+//     })
+//     let powerHourObj2 = await prisma.powerHour.create({
+//         data: {
+//             title: 'Power Hour 2',
+//             description: 'Power Hour 1',
+//             date_time: new Date('12-12-2023'),
+//             cover_image: 'https://res.cloudinary.com/dymmbugh2/image/upload/v1697937069/dtn-image/g2mkvb7takf9pojc4ium.webp',
+//             songLimit: 1,
+//             inviteToken: 'G7r5nDHx'
+//         }
+//     })
+//     await prisma.host.create({
+//         data: {
+//             userId: 2,
+//             powerHourId: powerHourObj2.id
+//         }
+//     })
+//     await prisma.participant.create({
+//         data: {
+//             userId: 2,
+//             powerHourId: powerHourObj2.id
+//         }
+//     })
+//     let powerHourObj3 = await prisma.powerHour.create({
+//         data: {
+//             title: 'Power Hour 3',
+//             description: 'Power Hour 1',
+//             date_time: new Date('12-12-2023'),
+//             cover_image: 'https://res.cloudinary.com/dymmbugh2/image/upload/v1697937069/dtn-image/g2mkvb7takf9pojc4ium.webp',
+//             songLimit: 1,
+//             inviteToken: '9trZK7Sf'
+//         }
+//     })
+//     await prisma.host.create({
+//         data: {
+//             userId: 2,
+//             powerHourId: powerHourObj3.id
+//         }
+//     })
+//     await prisma.participant.create({
+//         data: {
+//             userId: 2,
+//             powerHourId: powerHourObj3.id
+//         }
+//     })
+//     // let songs = [
+//     //     {
+//     //         title: faker.music.songName(),
+//     //         artist: "The Beatles",
+//     //         youtubeLink: 'https://www.youtube.com/watch?v=QGnkTQikhsE',
+//     //         startTime: '0:10',
+//     //         endTime: '0:15',
+//     //         powerHourId: 1,
+//     //         orderNumber: 1,
+//     //         participantId: 1
+//     //     },
+//     //     {
+//     //         title: faker.music.songName(),
+//     //         artist: "The Buggles",
+//     //         youtubeLink: 'https://www.youtube.com/watch?v=W8r-tXRLazs',
+//     //         startTime: '0:10',
+//     //         endTime: '0:15',
+//     //         powerHourId: 1,
+//     //         orderNumber: 2,
+//     //         participantId: 1
+//     //     },
+//     //     {
+//     //         title: faker.music.songName(),
+//     //         artist: "Brand New",
+//     //         youtubeLink: 'https://www.youtube.com/watch?v=qgtkPKZ2OPk',
+//     //         startTime: '0:10',
+//     //         endTime: '0:15',
+//     //         powerHourId: 1,
+//     //         orderNumber: 3,
+//     //         participantId: 1
+//     //     },
+//     //     {
+//     //         title: faker.music.songName(),
+//     //         artist: "random 1",
+//     //         youtubeLink: 'https://www.youtube.com/watch?v=qgtkPKZ2OPk',
+//     //         startTime: '0:10',
+//     //         endTime: '0:15',
+//     //         powerHourId: 1,
+//     //         orderNumber: 0,
+//     //         participantId: 1
+//     //     },
+//     //     {
+//     //         title: faker.music.songName(),
+//     //         artist: "random 2",
+//     //         youtubeLink: 'https://www.youtube.com/watch?v=qgtkPKZ2OPk',
+//     //         startTime: '0:10',
+//     //         endTime: '0:15',
+//     //         powerHourId: 1,
+//     //         orderNumber: 0,
+//     //         participantId: 1
+//     //     },
+//     //     {
+//     //         title: faker.music.songName(),
+//     //         artist: "random 3",
+//     //         youtubeLink: 'https://www.youtube.com/watch?v=qgtkPKZ2OPk',
+//     //         startTime: '0:10',
+//     //         endTime: '0:15',
+//     //         powerHourId: 1,
+//     //         orderNumber: 0,
+//     //         participantId: 1
+//     //     }
+//     // ]
+//     // console.log('creating songs')
+//     // for (let song of songs) {
+//     //     await prisma.powerHourSong.create({
+//     //         data: song
+//     //     })
+//     // }
 
-main()
-.then(async () => {
-    await prisma.$disconnect()
-})
-.catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-})
+//     // console.log('create invites')
+//     // await prisma.invite.create({
+//     //     data: {
+//     //         userId: 1,
+//     //         powerHourId: powerHourObj1.id
+//     //     }
+//     // })
+//     // await prisma.invite.create({
+//     //     data: {
+//     //         userId: 1,
+//     //         powerHourId: powerHourObj2.id
+//     //     }
+//     // })
+//     // await prisma.invite.create({
+//     //     data: {
+//     //         userId: 1,
+//     //         powerHourId: powerHourObj3.id
+//     //     }
+//     // })
+// }
+
+// main()
+// .then(async () => {
+//     await prisma.$disconnect()
+// })
+// .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+// })
