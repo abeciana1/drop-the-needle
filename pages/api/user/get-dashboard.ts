c6d00f83e9ecc85b8752ed3dd6cb20dbb8fba3d3
Alex <alex.beciana@gmail.com>
2023-11-12 15:02:08 -0500

    Added check to only allow post request

--- pages/api/user/get-dashboard.ts
+++ pages/api/user/get-dashboard.ts
@@ -1,66 +1,70 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/hooks/prisma'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    let user = await prisma?.user?.findUnique({
-        where: {
-            email: req?.body?.params
-        },
-        include: {
-            hosted: {
-                orderBy: {
-                    powerHour: {
-                        date_time: 'asc'
-                    }
-                },
-                select: {
-                    powerHour: {
-                        select: {
-                            id: true,
-                            title: true,
-                            cover_image: true,
-                            date_time: true
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST') {
+        let user = await prisma?.user?.findUnique({
+            where: {
+                email: req?.body?.params
+            },
+            include: {
+                hosted: {
+                    orderBy: {
+                        powerHour: {
+                            date_time: 'asc'
+                        }
+                    },
+                    select: {
+                        powerHour: {
+                            select: {
+                                id: true,
+                                title: true,
+                                cover_image: true,
+                                date_time: true
+                            }
                         }
                     }
-                }
-            },
-            participants: {
-                orderBy: {
-                    powerHour: {
-                        date_time: 'asc'
-                    }
                 },
-                select: {
-                    powerHour: {
-                        select: {
-                            id: true,
-                            title: true,
-                            cover_image: true,
-                            date_time: true
+                participants: {
+                    orderBy: {
+                        powerHour: {
+                            date_time: 'asc'
+                        }
+                    },
+                    select: {
+                        powerHour: {
+                            select: {
+                                id: true,
+                                title: true,
+                                cover_image: true,
+                                date_time: true
+                            }
                         }
                     }
-                }
-            },
-            invites: {
-                orderBy: {
-                    powerHour: {
-                        date_time: 'asc'
-                    }
                 },
-                include: {
-                    powerHour: {
-                        select: {
-                            id: true,
-                            title: true,
-                            description: true,
-                            cover_image: true,
-                            date_time: true,
-                            songLimit: true
+                invites: {
+                    orderBy: {
+                        powerHour: {
+                            date_time: 'asc'
+                        }
+                    },
+                    include: {
+                        powerHour: {
+                            select: {
+                                id: true,
+                                title: true,
+                                description: true,
+                                cover_image: true,
+                                date_time: true,
+                                songLimit: true
+                            }
                         }
                     }
                 }
             }
-        }
-    })
-    res.status(200).json({ user });
+        })
+        res.status(200).json({ user });
+    }
 }
+
+export default handler
