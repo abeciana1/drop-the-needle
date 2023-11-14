b3c9c7c9f138af82ff5f15aaa46c9b17844aea9b
Alex <alex.beciana@gmail.com>
2023-11-13 20:24:23 -0500

    Created time converted for track start and end times

--- utils/index.ts
+++ utils/index.ts
@@ -1,1 +1,12 @@
 export const generateRandomString = (length=8)=>Math.random().toString(20).substr(2, length)
+
+export const timeConverter = (time: string) => {
+    let timeSplit = time.split(':')
+    let seconds;
+    if (timeSplit.length === 3) {
+        seconds = (+timeSplit[0]) * 60 * 60 + (+timeSplit[1]) * 60 + (+timeSplit[2]); 
+    } else {
+        seconds = (+timeSplit[0]) * 60 + (+timeSplit[1]); 
+    }
+    return seconds
+}
