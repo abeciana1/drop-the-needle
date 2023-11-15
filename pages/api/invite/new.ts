305b294e2cec9497dcca1ae06b7f46394042ffce
Alex <alex.beciana@gmail.com>
2023-11-15 16:17:07 -0500

    Added extra condition to check ph publish status

--- pages/api/invite/new.ts
+++ pages/api/invite/new.ts
@@ -9,40 +9,46 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
             }
         })
         if (foundPowerHour) {
-            const isAfter = new Date(foundPowerHour?.submissionDeadline).valueOf() - new Date().valueOf() > 0
-            if (!isAfter) {
-                let foundInvite = await prisma.invite.findMany({
-                    where: {
-                        powerHourId: foundPowerHour.id,
-                        userId: req?.body?.userId
-                    }
-                })
-                let foundParticipant = await prisma.participant.findMany({
-                    where: {
-                        powerHourId: foundPowerHour.id,
-                        userId: req?.body?.userId
-                    }
-                })
-                if (!foundInvite[0] && !foundParticipant[0]) {
-                    let newInvite = await prisma.invite.create({
-                        data: {
-                            userId: req?.body?.userId,
-                            powerHourId: foundPowerHour.id
+            if (foundPowerHour.publishStatus) {
+                const isAfter = new Date(foundPowerHour?.submissionDeadline).valueOf() - new Date().valueOf() > 0
+                if (!isAfter) {
+                    let foundInvite = await prisma.invite.findMany({
+                        where: {
+                            powerHourId: foundPowerHour.id,
+                            userId: req?.body?.userId
                         }
                     })
-                    res.status(200).json({ newInvite })
-                } else if (foundParticipant[0]) {
-                    res.status(200).json({
-                        message: `You are already participating in the ${foundPowerHour.title} event`
+                    let foundParticipant = await prisma.participant.findMany({
+                        where: {
+                            powerHourId: foundPowerHour.id,
+                            userId: req?.body?.userId
+                        }
                     })
+                    if (!foundInvite[0] && !foundParticipant[0]) {
+                        let newInvite = await prisma.invite.create({
+                            data: {
+                                userId: req?.body?.userId,
+                                powerHourId: foundPowerHour.id
+                            }
+                        })
+                        res.status(200).json({ newInvite })
+                    } else if (foundParticipant[0]) {
+                        res.status(200).json({
+                            message: `You are already participating in the ${foundPowerHour.title} event`
+                        })
+                    } else {
+                        res.status(200).json({
+                            message: `You already have an invite to the ${foundPowerHour.title} event`
+                        })
+                    }
                 } else {
                     res.status(200).json({
-                        message: `You already have an invite to the ${foundPowerHour.title} event`
+                        message: `Sorry, you can't join the ${foundPowerHour.title} event because the submission deadline has passed.`
                     })
                 }
             } else {
                 res.status(200).json({
-                    message: `Sorry, you can't join the ${foundPowerHour.title} event because the submission deadline has passed.`
+                    message: 'Sorry, the power hour has not been published.'
                 })
             }
         }
