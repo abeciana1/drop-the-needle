0c6b21ef6782ffa8726d0de2893fefbe8d79e855
Alex <alex.beciana@gmail.com>
2023-11-15 21:48:04 -0500

    Added search filtering form and mapped over power hours

--- pages/listen/index.tsx
+++ pages/listen/index.tsx
@@ -1,13 +1,34 @@
-import { Fragment } from 'react'
-import { GetServerSidePropsContext } from 'next';
+import { Fragment, useMemo } from 'react'
 import {
     SEO,
     ComponentMargin,
     WavySection,
-    CommonPageLayout
+    CommonPageLayout,
+    PlaylistCardGroup,
+    PlaylistCard
 } from '@/components/common'
+import { H1, H2 } from '@/components/styled'
+import { Input } from '@/components/account'
+import { useForm, useWatch } from "react-hook-form"
+import axios from 'axios'
+import { PowerHourGroupI, DashPowerHourI } from '@/interfaces'
+import { formatInTimeZone } from 'date-fns-tz'
 
-const ListenIdx = () => {
+const ListenIdx = ({ powerHours }: any) => {
+    console.log('powerHours', powerHours)
+    const {
+        register,
+        control
+    } = useForm()
+    const searchWatch = useWatch({
+        control,
+        name: 'searchInput',
+        defaultValue: ''
+    })
+
+    const searchFilteredPhs = useMemo(() => {
+        return powerHours?.filter((powerHour: any) => powerHour?.title?.toLowerCase().includes(searchWatch.toLowerCase()))
+    }, [searchWatch])
     
     return(
         <Fragment>
@@ -16,10 +37,52 @@ const ListenIdx = () => {
                 description='Relive past power hours.'
             />
             <CommonPageLayout footerColor='vermillion-200'>
-                <ComponentMargin>
-
+                <ComponentMargin bgColor='altWhite'>
+                    <H1 color={0} text={'All Power Hours'} />
+                    <section className='py-3'>
+                        <Input
+                            hideLabel
+                            label='Search...'
+                            placeholder='Search...'
+                            name='searchInput'
+                            fieldRequired={false}
+                            register={register}
+                        />
+                    </section>
+                    <>
+                        {searchFilteredPhs?.length > 0 &&
+                            <>
+                                <PlaylistCardGroup>
+                                    {searchFilteredPhs?.map((powerHour: any) => {
+                                        return (
+                                            <PlaylistCard
+                                                key={powerHour.id}
+                                                id={powerHour.id}
+                                                title={powerHour.title}
+                                                cover_image={powerHour.cover_image}
+                                                date={formatInTimeZone(new Date(powerHour?.date_time), Intl.DateTimeFormat().resolvedOptions().timeZone, 'MM/dd/yyyy')}
+                                                time={formatInTimeZone(new Date(powerHour?.date_time), Intl.DateTimeFormat().resolvedOptions().timeZone, 'p zzz')}
+                                                publicLink
+                                                hostedLink={false}
+                                            />
+                                        )
+                                    })}
+                                </PlaylistCardGroup>
+                            </>
+                        }
+                        {searchFilteredPhs?.length < 1 &&
+                            <>
+                            {searchWatch.length < 1 && 
+                                <H2 text='Sorry there are no publicly listed power hours.' color={0} />
+                            }
+                            {searchWatch.length > 0 &&
+                                <H2 text='Sorry there are no power hours meeting that filter.' color={0} />
+                            }
+                            </>
+                        }
+                    </>
                 </ComponentMargin>
-                <WavySection color='ceruleanBlue' type={3} bgColor='vermillion-200' />
+                <WavySection color='altWhite' type={3} bgColor='vermillion-200' />
             </CommonPageLayout>
         </Fragment>
     )
@@ -27,9 +90,11 @@ const ListenIdx = () => {
 
 export default ListenIdx
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-
+export const getServerSideProps = async () => {
+    const { data } = await axios.get('http://localhost:3000/api/powerhour/get-all-public')
     return {
-        props: {}
+        props: {
+            powerHours: data?.publicPowerHours
+        }
     }
 }
