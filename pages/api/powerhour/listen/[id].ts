8b596d443eb79c40584d019a3be17be70ad72c0b
Alex <alex.beciana@gmail.com>
2023-11-16 17:10:26 -0500

    Created api endpoint to fetch power hour data for dynamic listen pages

---
+++ pages/api/powerhour/listen/[id].ts
@@ -0,0 +1,68 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/hooks/prisma'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'GET') {
+        const powerHour = await prisma?.powerHour?.findUnique({
+            where: {
+                id: Number(req?.query?.id)
+            },
+            select: {
+                id: true,
+                title: true,
+                cover_image: true,
+                description: true,
+                participants: {                
+                    select: {
+                        user: {
+                            select: {
+                                id: true,
+                                name: true
+                            }
+                        }
+                    }
+                },
+                hosts: {
+                    select: {
+                        user: {
+                            select: {
+                                id: true,
+                                name: true
+                            }
+                        }
+                    }
+                },
+                PowerHourSongs: {
+                    where: {
+                        orderNumber: {
+                            gt: 0
+                        }
+                    },
+                    select: {
+                        id: true,
+                        title: true,
+                        artist: true,
+                        youtubeLink: true,
+                        startTime: true,
+                        endTime: true,
+                        album: true,
+                        year: true,
+                        participant: {
+                            select: {
+                                user: {
+                                    select: {
+                                        id: true,
+                                        name: true
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        res.status(200).json({ powerHour })
+    }
+}
+
+export default handler
