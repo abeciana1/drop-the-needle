46019e831835ff219c7fbf2dfa0ccdcc82dede05
Alex <alex.beciana@gmail.com>
2023-11-17 14:32:34 -0500

    Removed unused icon import && Moved panel to left

--- components/common/Drawer/index.tsx
+++ components/common/Drawer/index.tsx
@@ -3,7 +3,7 @@ import { Dialog, Transition } from '@headlessui/react'
 import { ExpandBtn } from '@/components/common'
 import { AiOutlineClose } from 'react-icons/ai'
 import { SideNavItemButton } from '@/components/account'
-import { CiLogout, CiLogin } from "react-icons/ci";
+import { CiLogin } from "react-icons/ci";
 import { DrawerI } from '@/interfaces'
 
 const Drawer = ({
@@ -19,28 +19,28 @@ const Drawer = ({
     return(
         <Fragment>
             {!open &&
-                <div className='bg-altBlack absolute p-2 rounded-full right-0 top-1/2'>
+                <div className='bg-altBlack p-2 rounded-full left-0 top-1/2 fixed'>
                     <SideNavItemButton
-                        text='Open selection'
-                        icon={CiLogout}
+                        text='Open song selection'
+                        icon={CiLogin}
                         onClick={toggleOpen}
                     />
                 </div>
             }
             <Transition.Root show={open} as={Fragment}>
             <Dialog as="div" className="relative z-50" onClose={setOpen}>
-                <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <div className="pointer-events-none fixed inset-y-0 left-0 flex max-w-full">
                     <Transition.Child
                     as={Fragment}
                     enter="transform transition ease-in-out duration-500 sm:duration-700"
-                    enterFrom="translate-x-full"
+                    enterFrom="-translate-x-full"
                     enterTo="translate-x-0"
                     leave="transform transition ease-in-out duration-500 sm:duration-700"
                     leaveFrom="translate-x-0"
-                    leaveTo="translate-x-full"
+                    leaveTo="-translate-x-full"
                     >
                     <Dialog.Panel className="pointer-events-auto relative w-screen max-w-md">
-                        <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                        <div className="flex h-full flex-col overflow-y-auto bg-white py-6 shadow-xl">
                         <div className="px-4 sm:px-6 flex items-center justify-between">
                             <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
                                 {panelTitle}
